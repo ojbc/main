e420139b45a9e67296621f9221ff6546293ed5b9
ychawla <yogesh@search.org>
2015-02-25 09:24:03 -0600

    Fixed unit tests.

--- intermediaries/criminal-identification-reporting-service-intermediary/src/test/java/org/ojbc/bundles/intermediaries/CamelContextTest.java
+++ intermediaries/criminal-identification-reporting-service-intermediary/src/test/java/org/ojbc/bundles/intermediaries/CamelContextTest.java
@@ -1,22 +1,18 @@
 package org.ojbc.bundles.intermediaries;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.apache.camel.test.junit4.CamelSpringJUnit4ClassRunner;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 
-//TODO FIXME enable test when passing
-@Ignore
 @RunWith(CamelSpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
 		"classpath:META-INF/spring/camel-context.xml" })
 public class CamelContextTest {
 
-	//TODO FIXME enable test when passing
-	@Ignore
+	@Test
 	public void contextStartup() {
 		assertTrue(true);
 	}
