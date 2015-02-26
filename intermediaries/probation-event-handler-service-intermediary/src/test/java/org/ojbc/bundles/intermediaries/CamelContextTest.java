8ab4a9db4fb7852d87a175fd7d42a19c5cfc8656
ychawla <yogesh@search.org>
2015-02-26 15:25:05 -0600

    Initial cut of Probation Handler Intermediary main bundle.

---
+++ intermediaries/probation-event-handler-service-intermediary/src/test/java/org/ojbc/bundles/intermediaries/CamelContextTest.java
@@ -0,0 +1,24 @@
+package org.ojbc.bundles.intermediaries;
+
+import static org.junit.Assert.*;
+
+import org.apache.camel.test.junit4.CamelSpringJUnit4ClassRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+
+@RunWith(CamelSpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={
+		"classpath:META-INF/spring/camel-context.xml", 
+		"classpath:META-INF/spring/cxf-endpoints.xml",
+		"classpath:META-INF/spring/extensible-beans.xml",		
+		"classpath:META-INF/spring/local-osgi-context.xml",
+		"classpath:META-INF/spring/properties-context.xml"}) 
+public class CamelContextTest {
+
+	@Test
+	public void contextStartup() {
+		assertTrue(true);
+	}
+
+}
---
+++ intermediaries/probation-event-handler-service-intermediary/src/test/java/org/ojbc/xslt/ProbationTransformerServiceTest.java
@@ -0,0 +1,164 @@
+package org.ojbc.xslt;
+
+//import static org.hamcrest.Matchers.is;
+//import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.transform.sax.SAXSource;
+
+import org.apache.commons.io.FileUtils;
+import org.custommonkey.xmlunit.Diff;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+public class ProbationTransformerServiceTest {
+
+	XsltTransformerService unit;
+
+	@Before
+	public void setup() {
+		unit = new XsltTransformerService();		
+	}
+	
+	@After
+	public void tearDown() {
+		unit = null;
+	}	
+
+	@Test
+	public void probationSubscribeTransform() throws Exception{
+		String xml = FileUtils.readFileToString(new File( "src/test/resources/xmlInstances/probation/probationCaseInitiation.xml"));
+		String xslt = FileUtils.readFileToString(new File("src/main/resources/xslt/probationDocumentToSubscription.xsl"));
+
+		transformAndValidate(xslt, xml,"output/subscription.xml", getParams());
+	}
+	
+	@Test
+	public void probationSubscribeNonBiometricTransform() throws Exception{
+		String xml = FileUtils.readFileToString(new File( "src/test/resources/xmlInstances/probation/ProbationCase_nonBiometric.xml"));
+		String xslt = FileUtils.readFileToString(new File("src/main/resources/xslt/probationDocumentToSubscription.xsl"));
+
+		transformAndValidate(xslt, xml,"output/subscribe_nonBiometric.xml", getParams());
+	}
+	
+	@Test
+	public void probationSubscribeTransform_status1() throws Exception{
+		String xml = FileUtils.readFileToString(new File( "src/test/resources/xmlInstances/probation/ProbationCase_status1.xml"));
+		String xslt = FileUtils.readFileToString(new File("src/main/resources/xslt/probationDocumentToSubscription.xsl"));
+
+		transformAndValidate(xslt, xml,"output/subscribe_status1.xml", getParams());
+	}
+	
+	@Test
+	public void probationSubscribeTransform_status2() throws Exception{
+		String xml = FileUtils.readFileToString(new File( "src/test/resources/xmlInstances/probation/ProbationCase_status2.xml"));
+		String xslt = FileUtils.readFileToString(new File("src/main/resources/xslt/probationDocumentToSubscription.xsl"));
+
+		transformAndValidate(xslt, xml,"output/subscribe_status2.xml", getParams());
+	}
+	
+	@Test
+	public void probationSubscribeTransform_status3() throws Exception{
+		String xml = FileUtils.readFileToString(new File( "src/test/resources/xmlInstances/probation/ProbationCase_status3.xml"));
+		String xslt = FileUtils.readFileToString(new File("src/main/resources/xslt/probationDocumentToSubscription.xsl"));
+
+		transformAndValidate(xslt, xml,"output/subscribe_status3.xml", getParams());
+	}
+	
+	@Test
+	public void probationSubscribeTransform_status4() throws Exception{
+		String xml = FileUtils.readFileToString(new File( "src/test/resources/xmlInstances/probation/ProbationCase_status4.xml"));
+		String xslt = FileUtils.readFileToString(new File("src/main/resources/xslt/probationDocumentToSubscription.xsl"));
+
+		transformAndValidate(xslt, xml,"output/subscribe_status4.xml", getParams());
+	}
+	
+	@Test
+	public void probationSubscribeTransform_status5() throws Exception{
+		String xml = FileUtils.readFileToString(new File( "src/test/resources/xmlInstances/probation/ProbationCase_status5.xml"));
+		String xslt = FileUtils.readFileToString(new File("src/main/resources/xslt/probationDocumentToSubscription.xsl"));
+
+		transformAndValidate(xslt, xml,"output/subscribe_status5.xml", getParams());
+	}
+	
+	@Test
+	public void probationSubscribeTransform_status6() throws Exception{
+		String xml = FileUtils.readFileToString(new File( "src/test/resources/xmlInstances/probation/ProbationCase_status6.xml"));
+		String xslt = FileUtils.readFileToString(new File("src/main/resources/xslt/probationDocumentToSubscription.xsl"));
+
+		transformAndValidate(xslt, xml,"output/subscribe_status6.xml", getParams());
+	}
+		
+	@Test
+	public void probationUnSubscribeTransform_status7() throws Exception{
+		String xml = FileUtils.readFileToString(new File( "src/test/resources/xmlInstances/probation/ProbationCase_status7.xml"));
+		String xslt = FileUtils.readFileToString(new File("src/main/resources/xslt/probationDocumentToUnsubscription.xsl"));
+
+		transformAndValidate(xslt, xml,"output/unsubscribe_status7.xml", getParams());
+	}
+	
+	@Test
+	public void probationUnSubscribeTransform_nonBiometricStatus7() throws Exception{
+		String xml = FileUtils.readFileToString(new File( "src/test/resources/xmlInstances/probation/ProbationCase_nonBiometricStatus7.xml"));
+		String xslt = FileUtils.readFileToString(new File("src/main/resources/xslt/probationDocumentToUnsubscription.xsl"));
+
+		transformAndValidate(xslt, xml,"output/unsubscribe_nonBiometricStatus7.xml", getParams());
+	}
+	
+	@Test
+	public void probationUnSubscribeTransform_supervisionRelease() throws Exception{
+		String xml = FileUtils.readFileToString(new File( "src/test/resources/xmlInstances/probation/ProbationCase_supervisionRelease.xml"));
+		String xslt = FileUtils.readFileToString(new File("src/main/resources/xslt/probationDocumentToUnsubscription.xsl"));
+
+		transformAndValidate(xslt, xml,"output/unsubscribe_supervisionRelease.xml", getParams());
+	}
+	
+	@Test
+	public void probationUnsubscribeTransform() throws Exception{
+		String xml = FileUtils.readFileToString(new File( "src/test/resources/xmlInstances/probation/probationCaseTermination.xml"));
+		String xslt = FileUtils.readFileToString(new File("src/main/resources/xslt/probationDocumentToUnsubscription.xsl"));
+
+		transformAndValidate(xslt, xml,"output/unsubscription.xml", getParams());
+	}
+	
+	@SuppressWarnings("unchecked")
+	private void transformAndValidate(String xslPath, String inputXmlPath, String expectedHtmlPath, Map<String,Object> params) throws IOException {
+
+		String expectedXml = FileUtils.readFileToString(new File("src/test/resources/xmlInstances/"+expectedHtmlPath));
+		String convertResult = unit.transform(createSource(inputXmlPath), createSource(xslPath),params);
+		
+		System.out.println(convertResult);
+		
+		// XMLUnit comparison: 	
+		Diff myDiff = null;		
+		try {
+			myDiff = new Diff(expectedXml, convertResult);
+		} catch (SAXException e) {
+			e.printStackTrace();
+		}
+		
+		assertTrue("XML similar " + myDiff.toString(), myDiff.similar());
+		assertTrue("XML identical " + myDiff.toString(), myDiff.identical());		
+	}
+
+	private SAXSource createSource(String xml) {
+		InputSource inputSource = new InputSource(new ByteArrayInputStream(xml.getBytes()));
+		inputSource.setEncoding(org.apache.commons.lang.CharEncoding.UTF_8);
+		return new SAXSource(inputSource);
+	}
+
+	private Map<String, Object> getParams() {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("topicExpression", "theTopic");
+		return map;
+	}
+}
