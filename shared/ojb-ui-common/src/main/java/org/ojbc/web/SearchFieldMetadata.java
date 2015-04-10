57ffcd212887b8d112b2554238cb6b2f21a8f106
Haiqi Wei <haiqi@search.org>
2015-04-10 10:00:05 -0500

    Change enum StartsWith's metadata to "startsWith".

--- shared/ojb-ui-common/src/main/java/org/ojbc/web/SearchFieldMetadata.java
+++ shared/ojb-ui-common/src/main/java/org/ojbc/web/SearchFieldMetadata.java
@@ -20,12 +20,12 @@ package org.ojbc.web;
  * This class assigns enumerations to static values from 
  * the UI layer.
  * 
- * A use case for this funcationality is translating from POJO to XML
+ * A use case for this functionality is translating from POJO to XML
  * 
  */
 public enum SearchFieldMetadata {
 
-	StartsWith("StartsWith"), Partial("partial"), ExactMatch("exact");
+	StartsWith("startsWith"), Partial("partial"), ExactMatch("exact");
 
 	private String metadata;
 
