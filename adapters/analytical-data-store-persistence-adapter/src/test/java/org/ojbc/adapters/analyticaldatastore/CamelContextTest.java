9508f64cb4e824d5d18f7c52e4e0bb40fcebea44
ychawla <yogesh@search.org>
2015-05-27 13:42:08 -0500

    Added license info, updated POM with parent POM declaration, and incremental check-in.

--- adapters/analytical-data-store-persistence-adapter/src/test/java/org/ojbc/adapters/analyticaldatastore/CamelContextTest.java
+++ adapters/analytical-data-store-persistence-adapter/src/test/java/org/ojbc/adapters/analyticaldatastore/CamelContextTest.java
@@ -1,3 +1,19 @@
+/*
+ * Unless explicitly acquired and licensed from Licensor under another license, the contents of
+ * this file are subject to the Reciprocal Public License ("RPL") Version 1.5, or subsequent
+ * versions as allowed by the RPL, and You may not copy or use this file in either source code
+ * or executable form, except in compliance with the terms and conditions of the RPL
+ *
+ * All software distributed under the RPL is provided strictly on an "AS IS" basis, WITHOUT
+ * WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND LICENSOR HEREBY DISCLAIMS ALL SUCH
+ * WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific language
+ * governing rights and limitations under the RPL.
+ *
+ * http://opensource.org/licenses/RPL-1.5
+ *
+ * Copyright 2012-2015 Open Justice Broker Consortium
+ */
 package org.ojbc.adapters.analyticaldatastore;
 
 import static org.junit.Assert.assertNotNull;
@@ -21,6 +37,7 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Produce;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.AdviceWithRouteBuilder;
+import org.apache.camel.component.cxf.common.message.CxfConstants;
 import org.apache.camel.impl.DefaultExchange;
 import org.apache.camel.model.ModelCamelContext;
 import org.apache.camel.test.junit4.CamelSpringJUnit4ClassRunner;
@@ -30,6 +47,7 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.cxf.binding.soap.SoapHeader;
 import org.apache.cxf.endpoint.Client;
 import org.apache.cxf.headers.Header;
+import org.apache.cxf.message.MessageImpl;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -72,7 +90,92 @@ public class CamelContextTest {
     	    }              
     	});
 
+    	//We replace the 'from' web service endpoint with a direct endpoint we call in our test
+    	context.getRouteDefinition("Pretrial_Service_Enrollment_Reporting_Service_Route").adviceWith(context, new AdviceWithRouteBuilder() {
+    	    @Override
+    	    public void configure() throws Exception {
+    	    	// The line below allows us to bypass CXF and send a message directly into the route
+    	    	replaceFromWith("direct:pretrialEnrollmentReportingServiceEndpoint");
+    	    }              
+    	});
+
+    	context.getRouteDefinition("Disposition_Reporting_Service_Route").adviceWith(context, new AdviceWithRouteBuilder() {
+    	    @Override
+    	    public void configure() throws Exception {
+    	    	// The line below allows us to bypass CXF and send a message directly into the route
+    	    	replaceFromWith("direct:dispositionReportingServiceEndpoint");
+    	    }              
+    	});
+
+    	
     	context.start();
 	}	
 	
+	@Test
+	public void testIncidentReportService() throws Exception
+	{
+    	Exchange incidentReportExchange = createSenderExchange("src/test/resources/xmlInstances/incidentReport/incidentReport.xml");
+	    
+	    //Send the one-way exchange.  Using template.send will send an one way message
+		Exchange returnExchange = template.send("direct:incidentReportingServiceEndpoint", incidentReportExchange);
+		
+		//Use getException to see if we received an exception
+		if (returnExchange.getException() != null)
+		{	
+			throw new Exception(returnExchange.getException());
+		}	
+
+		//Sleep while a response is generated
+		Thread.sleep(3000);
+	    
+
+	}
+
+	protected Exchange createSenderExchange(String pathToInputFile) throws Exception, IOException {
+		//Create a new exchange
+    	Exchange incidentReportExchange = new DefaultExchange(context);
+
+    	//Set the WS-Address Message ID
+		Map<String, Object> requestContext = OJBUtils.setWSAddressingMessageID("123456789");
+		
+		//Set the operation name and operation namespace for the CXF exchange
+		incidentReportExchange.getIn().setHeader(Client.REQUEST_CONTEXT , requestContext);
+		
+		Document doc = createDocument();
+		List<SoapHeader> soapHeaders = new ArrayList<SoapHeader>();
+		soapHeaders.add(makeSoapHeader(doc, "http://www.w3.org/2005/08/addressing", "MessageID", "12345"));
+		incidentReportExchange.getIn().setHeader(Header.HEADER_LIST , soapHeaders);
+		
+		org.apache.cxf.message.Message message = new MessageImpl();
+
+		incidentReportExchange.getIn().setHeader(CxfConstants.CAMEL_CXF_MESSAGE, message);
+    	
+	    //Read the firearm search request file from the file system
+	    File inputFile = new File(pathToInputFile);
+	    String inputStr = FileUtils.readFileToString(inputFile);
+
+	    assertNotNull(inputStr);
+	    
+	    log.debug(inputStr);
+	    
+	    //Set it as the message message body
+	    incidentReportExchange.getIn().setBody(inputStr);
+		return incidentReportExchange;
+	}
+	
+	private SoapHeader makeSoapHeader(Document doc, String namespace, String localName, String value) {
+		Element messageId = doc.createElementNS(namespace, localName);
+		messageId.setTextContent(value);
+		SoapHeader soapHeader = new SoapHeader(new QName(namespace, localName), messageId);
+		return soapHeader;
+	}	
+	
+	public static Document createDocument() throws Exception{
+
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		dbf.setNamespaceAware(true);
+		Document doc = dbf.newDocumentBuilder().newDocument();
+
+		return doc;
+	}
 }
