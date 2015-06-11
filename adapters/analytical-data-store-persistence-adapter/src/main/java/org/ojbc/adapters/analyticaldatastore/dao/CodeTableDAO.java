e081a668e17984c84c1be83850b8eb10ff04ccf4
Haiqi Wei <haiqi@search.org>
2015-06-11 10:52:20 -0500

    Organized imports.

--- adapters/analytical-data-store-persistence-adapter/src/main/java/org/ojbc/adapters/analyticaldatastore/dao/CodeTableDAO.java
+++ adapters/analytical-data-store-persistence-adapter/src/main/java/org/ojbc/adapters/analyticaldatastore/dao/CodeTableDAO.java
@@ -18,8 +18,6 @@ package org.ojbc.adapters.analyticaldatastore.dao;
 
 import java.util.List;
 
-import net.sf.ehcache.transaction.xa.processor.XARequest.RequestType;
-
 import org.ojbc.adapters.analyticaldatastore.dao.model.CodeTable;
 import org.ojbc.adapters.analyticaldatastore.dao.model.KeyValue;
 
