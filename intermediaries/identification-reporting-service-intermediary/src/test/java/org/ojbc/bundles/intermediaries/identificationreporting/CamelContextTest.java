1ac3aa5b2bbca63b47bf5b115d8f8cd67893ddfc
Haiqi Wei <haiqi@search.org>
2015-06-23 15:39:25 -0500

    Initial check in.

---
+++ intermediaries/identification-reporting-service-intermediary/src/test/java/org/ojbc/bundles/intermediaries/identificationreporting/CamelContextTest.java
@@ -0,0 +1,122 @@
+/*
+ * Unless explicitly acquired and licensed from Licensor under another license, the contents of
+ * this file are subject to the Reciprocal Public License ("RPL") Version 1.5, or subsequent
+ * versions as allowed by the RPL, and You may not copy or use this file in either source code
+ * or executable form, except in compliance with the terms and conditions of the RPL
+ *
+ * All software distributed under the RPL is provided strictly on an "AS IS" basis, WITHOUT
+ * WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND LICENSOR HEREBY DISCLAIMS ALL SUCH
+ * WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific language
+ * governing rights and limitations under the RPL.
+ *
+ * http://opensource.org/licenses/RPL-1.5
+ *
+ * Copyright 2012-2015 Open Justice Broker Consortium
+ */
+package org.ojbc.bundles.intermediaries.identificationreporting;
+
+import static org.junit.Assert.assertTrue;
+
+import javax.annotation.Resource;
+import javax.xml.namespace.QName;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.apache.camel.EndpointInject;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.AdviceWithRouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.model.ModelCamelContext;
+import org.apache.camel.test.junit4.CamelSpringJUnit4ClassRunner;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.cxf.binding.soap.SoapHeader;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+@RunWith(CamelSpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+		"classpath:META-INF/spring/camel-context.xml",
+		"classpath:META-INF/spring/cxf-endpoints.xml",		
+		"classpath:META-INF/spring/properties-context.xml",
+		"classpath:META-INF/spring/jetty-server.xml",
+		})
+public class CamelContextTest {
+
+	private static final Log log = LogFactory.getLog( CamelContextTest.class );
+	
+	public static final String CXF_OPERATION_NAME = "ReportPersonIdentificationRequest";
+	public static final String CXF_OPERATION_NAMESPACE = "http://ojbc.org/Services/WSDL/IdentificationReportingService/1.0";
+	
+    @Resource
+    private ModelCamelContext context;
+	
+    @Produce
+    protected ProducerTemplate template;
+    
+    @EndpointInject(uri = "mock:cxf:bean:IdentificationRecordingService")
+    protected MockEndpoint identificationRecordingServiceMock;
+    
+    @Before
+	public void setUp() throws Exception {
+		
+    	//We replace the 'from' web service endpoint with a direct endpoint we call in our test
+    	context.getRouteDefinition("IdentificationReportingServiceHandlerRoute").adviceWith(context, new AdviceWithRouteBuilder() {
+    	    @Override
+    	    public void configure() throws Exception {
+    	    	// The line below allows us to bypass CXF and send a message directly into the route
+    	    	replaceFromWith("direct:IdentificationReportingServiceEndpoint");
+    	    }              
+    	});
+
+    	//We mock the 'N-DEx' web service endpoint and intercept any submissions
+    	context.getRouteDefinition("CallIdentificationRecordingServiceRoute").adviceWith(context, new AdviceWithRouteBuilder() {
+    	    @Override
+    	    public void configure() throws Exception {
+    	    	// The line below allows us to bypass CXF and send a message directly into the route
+    	    	mockEndpointsAndSkip("cxf:bean:IdentificationRecordingService*");
+    	    }              
+    	});
+
+		context.start();		
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void contextStartup() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testContextRoutes() throws Exception
+	{
+		
+	}
+	
+	private SoapHeader makeSoapHeader(Document doc, String namespace, String localName, String value) {
+		Element messageId = doc.createElementNS(namespace, localName);
+		messageId.setTextContent(value);
+		SoapHeader soapHeader = new SoapHeader(new QName(namespace, localName), messageId);
+		return soapHeader;
+	}		
+	
+	public static Document createDocument() throws Exception{
+
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		dbf.setNamespaceAware(true);
+		Document doc = dbf.newDocumentBuilder().newDocument();
+
+		return doc;
+	}
+	
+}
