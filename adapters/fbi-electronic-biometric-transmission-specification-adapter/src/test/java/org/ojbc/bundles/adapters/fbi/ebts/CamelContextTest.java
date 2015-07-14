a60b153dc9a1d664488af6b3ca5e52cc98067a88
criffle <criffle@search.org>
2015-07-14 17:53:21 -0400

    developed fbi ebts camel context test to read properties from cfg file

--- adapters/fbi-electronic-biometric-transmission-specification-adapter/src/test/java/org/ojbc/bundles/adapters/fbi/ebts/CamelContextTest.java
+++ adapters/fbi-electronic-biometric-transmission-specification-adapter/src/test/java/org/ojbc/bundles/adapters/fbi/ebts/CamelContextTest.java
@@ -16,11 +16,39 @@
  */
 package org.ojbc.bundles.adapters.fbi.ebts;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.xml.namespace.QName;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.AdviceWithRouteBuilder;
+import org.apache.camel.impl.DefaultExchange;
+import org.apache.camel.model.ModelCamelContext;
 import org.apache.camel.test.junit4.CamelSpringJUnit4ClassRunner;
+import org.apache.commons.io.FileUtils;
+import org.apache.cxf.binding.soap.SoapHeader;
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.headers.Header;
+import org.apache.log4j.Logger;
+import org.custommonkey.xmlunit.DetailedDiff;
+import org.custommonkey.xmlunit.Diff;
+import org.custommonkey.xmlunit.XMLUnit;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.ojbc.util.camel.helper.OJBUtils;
 import org.springframework.test.context.ContextConfiguration;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 @RunWith(CamelSpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
@@ -29,10 +57,98 @@ import org.springframework.test.context.ContextConfiguration;
         "classpath:META-INF/spring/properties-context.xml",
         })
 public class CamelContextTest {
+	
+	private static final Logger logger = Logger.getLogger(CamelContextTest.class);
 	    
+    @Resource
+    private ModelCamelContext context;
+    
+    @Produce
+    protected ProducerTemplate producerTemplate;
+	
+    @Before
+    public void setup() throws Exception{
+    	
+		XMLUnit.setIgnoreWhitespace(true);
+    	XMLUnit.setIgnoreAttributeOrder(true);
+    	XMLUnit.setIgnoreComments(true);
+    	XMLUnit.setXSLTVersion("2.0");
+    	
+    	// replace'from' web service endpoint with a direct endpoint we call in our test
+    	context.getRouteDefinition("fbiEbtsServiceRoute").adviceWith(context, new AdviceWithRouteBuilder() {
+    	    @Override
+    	    public void configure() throws Exception {
+    	    	
+    	    	// bypass CXF and send a message directly into the route
+    	    	replaceFromWith("direct:fbiEbtsInputEndpoint");    	    	
+    	    }              
+    	}); 
+    	
+    	context.start();	
+    }
+    
 	@Test
 	public void contextStartup() {
 		Assert.assertTrue(true);
 	}
+	
+	
+	@Test
+	public void exchangeTest() throws Exception{
+		
+    	Exchange senderExchange = new DefaultExchange(context);
+
+	    File inputFile = new File("src/test/resources/input/OJBC_Subscription_Document.xml");
+	    String inputStr = FileUtils.readFileToString(inputFile);
+	    
+	    Assert.assertNotNull(inputStr);
+	    
+	    logger.info(inputStr);
+	    
+	    senderExchange.getIn().setBody(inputStr);	    
+	    	    
+	    	    
+		Map<String, Object> requestContext = OJBUtils.setWSAddressingMessageID("123456789");		
+		//Set the operation name and operation namespace for the CXF exchange
+		senderExchange.getIn().setHeader(Client.REQUEST_CONTEXT , requestContext);				
+		List<SoapHeader> soapHeaders = new ArrayList<SoapHeader>();
+		soapHeaders.add(makeSoapHeader("http://www.w3.org/2005/08/addressing", "MessageID", "12345"));		
+		senderExchange.getIn().setHeader(Header.HEADER_LIST , soapHeaders);
+		
+		
+	    Exchange returnExchange = producerTemplate.send("direct:fbiEbtsInputEndpoint", senderExchange);
+	    
+		if (returnExchange.getException() != null) {
+			throw new Exception(returnExchange.getException());
+		}
+		
+		String transformedReturnMessage = returnExchange.getIn().getBody(String.class);
+		
+		logger.info("return message: \n" + transformedReturnMessage);
+		
+		
+		//assert the transformed xml against expected xml output doc				
+		String expectedXmlString = FileUtils.readFileToString(
+				new File("src/test/resources/output/EBTS-RapBack-Criminal-Subscription-Request.xml"));
+							
+		Diff diff = XMLUnit.compareXML(expectedXmlString, transformedReturnMessage);		
+		
+		DetailedDiff detailedDiff = new DetailedDiff(diff);
+		
+		Assert.assertEquals(detailedDiff.toString(), 0, detailedDiff.getAllDifferences().size());
+	    
+	}
+	
+	private SoapHeader makeSoapHeader(String namespace, String localName, String value) throws ParserConfigurationException {
+		
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		dbf.setNamespaceAware(true);
+		Document doc = dbf.newDocumentBuilder().newDocument();
+		
+		Element messageId = doc.createElementNS(namespace, localName);
+		messageId.setTextContent(value);
+		SoapHeader soapHeader = new SoapHeader(new QName(namespace, localName), messageId);
+		return soapHeader;
+	}	
 
 }
