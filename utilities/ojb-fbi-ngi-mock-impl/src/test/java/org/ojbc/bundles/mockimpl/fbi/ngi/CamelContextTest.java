c51179ee9470d64d36af41f6ed1c46d541eb1e7a
criffle <criffle@search.org>
2015-07-21 13:29:06 -0400

    removed cxf bean for server side ssl since karaf provides it

--- utilities/ojb-fbi-ngi-mock-impl/src/test/java/org/ojbc/bundles/mockimpl/fbi/ngi/CamelContextTest.java
+++ utilities/ojb-fbi-ngi-mock-impl/src/test/java/org/ojbc/bundles/mockimpl/fbi/ngi/CamelContextTest.java
@@ -18,12 +18,10 @@ package org.ojbc.bundles.mockimpl.fbi.ngi;
 
 import org.apache.camel.test.junit4.CamelSpringJUnit4ClassRunner;
 import org.junit.Assert;
-import org.junit.Ignore;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 
-//TODO enable when passing
-@Ignore
 @RunWith(CamelSpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
         "classpath:META-INF/spring/camel-context.xml",
@@ -32,7 +30,7 @@ import org.springframework.test.context.ContextConfiguration;
         })
 public class CamelContextTest {
 	
-	@Ignore
+	@Test
 	public void testConextStartup(){
 		
 		Assert.assertTrue(true);
