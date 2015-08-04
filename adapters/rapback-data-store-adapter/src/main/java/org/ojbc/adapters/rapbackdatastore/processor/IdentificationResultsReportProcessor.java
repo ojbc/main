e999b07e36f81c7af1899ee1653012ba4758bb2a
Haiqi Wei <haiqi@search.org>
2015-08-04 15:58:42 -0500

    1. Add processing for FBI/STATE civil/criminal results. 2. Refactor the IdentificationRequestReportProcessor and AbstractReportRepositoryProcessor to move up several methods to share them. 3. Add updateSubject() method in RapbackDAO.

--- adapters/rapback-data-store-adapter/src/main/java/org/ojbc/adapters/rapbackdatastore/processor/IdentificationResultsReportProcessor.java
+++ adapters/rapback-data-store-adapter/src/main/java/org/ojbc/adapters/rapbackdatastore/processor/IdentificationResultsReportProcessor.java
@@ -16,13 +16,23 @@
  */
 package org.ojbc.adapters.rapbackdatastore.processor;
 
+import java.io.IOException;
+
 import org.apache.camel.Body;
 import org.apache.camel.Exchange;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.ojbc.adapters.rapbackdatastore.dao.model.CivilInitialRapSheet;
+import org.ojbc.adapters.rapbackdatastore.dao.model.CivilInitialResults;
+import org.ojbc.adapters.rapbackdatastore.dao.model.CriminalInitialResults;
+import org.ojbc.adapters.rapbackdatastore.dao.model.IdentificationTransaction;
+import org.ojbc.adapters.rapbackdatastore.dao.model.Subject;
+import org.ojbc.util.xml.XmlUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 
 @Service
 public class IdentificationResultsReportProcessor extends AbstractReportRepositoryProcessor {
@@ -33,6 +43,112 @@ public class IdentificationResultsReportProcessor extends AbstractReportReposito
 	public void processReport(@Body Document report, Exchange exchange) throws Exception
 	{
 		log.info("Processing Identification Results Report.");
+		
+		Node rootNode = XmlUtils.xPathNodeSearch(report, 
+				"/pidresults:PersonFederalIdentificationResults[ident-ext:CivilIdentificationReasonCode]|"
+				+ "/pidresults:PersonStateIdentificationResults[ident-ext:CivilIdentificationReasonCode]");
+		
+		if (rootNode != null){
+			processCivilInitialResultsReport(rootNode, exchange);
+		}
+		else{
+			rootNode = XmlUtils.xPathNodeSearch(report, 
+					"/pidresults:PersonFederalIdentificationResults[ident-ext:CriminalIdentificationReasonCode]|"
+							+ "/pidresults:PersonStateIdentificationResults[ident-ext:CriminalIdentificationReasonCode]");
+			processCriminalInitialResultsReport(rootNode, exchange); 		
+		}
+	}
+
+	private void processCriminalInitialResultsReport(Node rootNode, Exchange exchange) throws Exception {
+		String transactionNumber = getTransactionNumber(rootNode);
+		processIdentificationTransaction(rootNode, transactionNumber);
+		processCriminalInitialResults(rootNode, exchange, transactionNumber); 
+		
+	}
+
+	private void processCriminalInitialResults(Node rootNode, Exchange exchange, String transactionNumber) 
+			throws Exception {
+		CriminalInitialResults criminalInitialResults = new CriminalInitialResults(); 
+		criminalInitialResults.setTransactionNumber(transactionNumber);
+		
+		String attachmentId = getAttachmentId(rootNode);
+		criminalInitialResults.setMatch(StringUtils.isNotBlank(attachmentId));
+		
+		criminalInitialResults.setTransactionType("Transaction Type"); //TODO replace the placeholder with real value
+		//String rapBackCategory = XmlUtils.xPathStringSearch(rootNode, "ident-ext:CriminalIdentificationReasonCode");
+		if (rootNode.getLocalName().equals("PersonFederalIdentificationResults")){
+			criminalInitialResults.setResultsSender("FBI");
+		}
+		else{
+			criminalInitialResults.setResultsSender("STATE");
+		}
+		
+		rapbackDAO.saveCriminalInitialResults(criminalInitialResults);
+
+	}
+
+	private void processCivilInitialResultsReport(Node rootNode, Exchange exchange) throws Exception {
+		String transactionNumber = getTransactionNumber(rootNode);
+		
+		updateSubject(rootNode, transactionNumber);
+		processCivilInitialResults(rootNode, exchange, transactionNumber);  
+	}
+
+	private void processCivilInitialResults(Node rootNode, Exchange exchange,
+			String transactionNumber) throws Exception, IOException {
+		CivilInitialResults civilInitialResults = new CivilInitialResults(); 
+		civilInitialResults.setTransactionNumber(transactionNumber);
+		
+		String attachmentId = getAttachmentId(rootNode);
+		
+		civilInitialResults.setMatch(StringUtils.isNotBlank(attachmentId));
+			
+		civilInitialResults.setCurrentState("current_state"); //TODO replace the placeholder with real value
+		civilInitialResults.setTransactionType("Transaction Type"); //TODO replace the placeholder with real value
+		
+		String rapBackCategory = XmlUtils.xPathStringSearch(rootNode, "ident-ext:CivilIdentificationReasonCode");
+		civilInitialResults.setCivilRapBackCategory(rapBackCategory);
+		
+		if (rootNode.getLocalName().equals("PersonFederalIdentificationResults")){
+			civilInitialResults.setResultsSender("FBI");
+		}
+		else{
+			civilInitialResults.setResultsSender("STATE");
+		}
+		
+		Integer initalResultsPkId = rapbackDAO.saveCivilInitialResults(civilInitialResults);
+		
+		CivilInitialRapSheet civilInitialRapSheet = new CivilInitialRapSheet();
+		civilInitialRapSheet.setCivilIntitialResultId(initalResultsPkId);
+		civilInitialRapSheet.setTransactionType("Transaction Type"); //TODO replace the placeholder with real value.
+		
+		byte[] receivedAttachment = getAttachment(exchange, transactionNumber,
+				attachmentId);
+
+		civilInitialRapSheet.setRapSheet(receivedAttachment);
+		
+		rapbackDAO.saveCivilInitialRapSheet(civilInitialRapSheet);
+	}
+
+	private String getTransactionNumber(Node rootNode) throws Exception {
+		String transactionNumber = XmlUtils.xPathStringSearch(rootNode, "ident-ext:TransactionIdentification/nc30:IdentificationID");
+		return transactionNumber;
+	}
+
+	private void updateSubject(Node rootNode, String transactionNumber)
+			throws Exception {
+		IdentificationTransaction identificationTransaction = 
+				rapbackDAO.getIdentificationTransaction(transactionNumber); 
+		
+		Subject subject = identificationTransaction.getSubject(); 
+		String fbiId = XmlUtils.xPathStringSearch(rootNode, 
+				"jxdm50:Subject/nc30:RoleOfPerson/jxdm50:PersonAugmentation/jxdm50:PersonFBIIdentification/nc30:IdentificationID");
+		subject.setUcn(fbiId);
+		
+		String civilSid = XmlUtils.xPathStringSearch(rootNode, 
+				"jxdm50:Subject/nc30:RoleOfPerson/jxdm50:PersonAugmentation/jxdm50:PersonStateFingerprintIdentification/nc30:IdentificationID");
+		subject.setCivilSid(civilSid);
+		rapbackDAO.updateSubject(subject);
 	}
 
 }
--- adapters/rapback-data-store-adapter/src/test/java/org/ojbc/adapters/rapbackdatastore/TestIdentficationRecordingAndResponse.java
+++ adapters/rapback-data-store-adapter/src/test/java/org/ojbc/adapters/rapbackdatastore/TestIdentficationRecordingAndResponse.java
@@ -55,6 +55,8 @@ import org.apache.cxf.message.MessageImpl;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.ojbc.adapters.rapbackdatastore.dao.RapbackDAO;
+import org.ojbc.adapters.rapbackdatastore.dao.model.IdentificationTransaction;
 import org.ojbc.adapters.rapbackdatastore.processor.IdentificationRequestReportProcessor;
 import org.ojbc.util.camel.helper.OJBUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -62,6 +64,7 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
 
 @RunWith(CamelSpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
@@ -73,11 +76,17 @@ import org.w3c.dom.Element;
         "classpath:META-INF/spring/h2-mock-database-application-context.xml",
         "classpath:META-INF/spring/h2-mock-database-context-rapback-datastore.xml"
       })
-@DirtiesContext
 public class TestIdentficationRecordingAndResponse {
 	
+	private static final String CIVIL_RESULTS_ATTACHEMNT_ID = "http://ojbc.org/identification/results/example";
+
 	private static final Log log = LogFactory.getLog( TestIdentficationRecordingAndResponse.class );
+
+	private static final String TRANSACTION_NUMBER = "000001820140729014008340000";
 	
+	@Autowired
+	RapbackDAO rapbackDAO;
+
 	@Autowired
 	IdentificationRequestReportProcessor identificationRequestReportProcessor;
 	
@@ -149,9 +158,14 @@ public class TestIdentficationRecordingAndResponse {
 	}
 	
 	@Test
-	@DirtiesContext
-	public void testIdentificationRecordingRequestServiceSuccess() throws Exception
+	public void testIdentificationRecordingRequestAndResultsSuccess() throws Exception
 	{
+		civilRecordingRequestSuccess(); 
+		civilRecordingResultServiceSuccess();
+	}
+
+	private void civilRecordingRequestSuccess() throws Exception, IOException,
+			InterruptedException, SAXException {
 		Exchange senderExchange = createSenderExchange("src/test/resources/xmlInstances/identificationReport/person_identification_fbi_request-civil.xml");
 		
 		senderExchange.getIn().setHeader("operationName", "RecordPersonFederalIdentificationRequest");
@@ -160,7 +174,7 @@ public class TestIdentficationRecordingAndResponse {
 		 * add MTOM attachment to the exchange.
 		 */
 		
-		byte[] imgData = extractBytes("src/test/resources/xmlInstances/mtomAttachment/java.jpg");
+		byte[] imgData = extractImageBytes("src/test/resources/xmlInstances/mtomAttachment/java.jpg");
 		senderExchange.getIn().addAttachment("http://ojbc.org/identification/request/example", 
 			new DataHandler(new ByteArrayDataSource(imgData, "image/jpeg")));
 
@@ -181,15 +195,26 @@ public class TestIdentficationRecordingAndResponse {
 		String body = OJBUtils.getStringFromDocument(receivedExchange.getIn().getBody(Document.class));
 		assertAsExpected(body, "src/test/resources/xmlInstances/identificationReportingResponse/person_identification_report_success_response.xml");
 		
+		IdentificationTransaction identificationTransaction = 
+				rapbackDAO.getIdentificationTransaction(TRANSACTION_NUMBER); 
+		
+		assertNotNull(identificationTransaction); 
+		assertNotNull(identificationTransaction.getSubject());
 	}
 	
-	@Test
-	@DirtiesContext
-	public void testIdentificationRecordingResultServiceSuccess() throws Exception
+	public void civilRecordingResultServiceSuccess() throws Exception
 	{
 		Exchange senderExchange = createSenderExchange("src/test/resources/xmlInstances/identificationReport/person_identification_results_fbi_identification-civil.xml");
 		
 		senderExchange.getIn().setHeader("operationName", "RecordPersonFederalIdentificationResults");
+		
+		/*
+		 * add MTOM attachment to the exchange.
+		 */
+		
+		byte[] rapsheet = FileUtils.readFileToByteArray(new File("src/test/resources/xmlInstances/mtomAttachment/fbiResultFromLOTC.html"));
+		senderExchange.getIn().addAttachment(CIVIL_RESULTS_ATTACHEMNT_ID, 
+			new DataHandler(new ByteArrayDataSource(rapsheet, "text/plain")));
 		
 		//Send the one-way exchange.  Using template.send will send an one way message
 		Exchange returnExchange = template.send("direct:identificationRecordingServiceEndpoint", senderExchange);
@@ -200,7 +225,7 @@ public class TestIdentficationRecordingAndResponse {
 			throw new Exception(returnExchange.getException());
 		}	
 		
-		identificationReportingResultMessageProcessor.expectedMessageCount(1);
+		identificationReportingResultMessageProcessor.expectedMessageCount(2);
 		
 		identificationReportingResultMessageProcessor.assertIsSatisfied();
 		
@@ -257,7 +282,7 @@ public class TestIdentficationRecordingAndResponse {
 		return doc;
 	}
 	
-	public byte[] extractBytes(String ImageName) throws IOException {
+	public byte[] extractImageBytes(String ImageName) throws IOException {
 		// open image
 		File imgPath = new File(ImageName);
 		BufferedImage bufferedImage = ImageIO.read(imgPath);
