337921580944bf86df1b7514dda5c7da6d097945
Haiqi Wei <haiqi@search.org>
2015-09-03 09:30:22 -0500

    Add subscripiton info to the rapback search response.

--- utilities/h2-mock-database/src/main/resources/sql-rapback-datastore/h2-test-data.sql
+++ utilities/h2-mock-database/src/main/resources/sql-rapback-datastore/h2-test-data.sql
@@ -19,8 +19,8 @@
 insert into subscription(id, topic, startDate, endDate, lastValidationDate, subscribingSystemIdentifier, subscriptionOwner, subjectName, active) values ('62720','{http://ojbc.org/wsn/topics}:person/incident', '2011-10-19', NULL, '2011-10-19', '{http://demostate.gov/SystemNames/1.0}SystemC', 'OJBC:IDP:OJBC:USER:admin', 'MICHAEL Smith-Jones', '1');
 insert into subscription(id, topic, startDate, endDate, lastValidationDate, subscribingSystemIdentifier, subscriptionOwner, subjectName, active) values ('62721','{http://ojbc.org/wsn/topics}:person/incident', '2011-10-19', NULL, '2011-10-19', '{http://demostate.gov/SystemNames/1.0}SystemC', 'OJBC:IDP:OJBC:USER:admin', 'MICHAEL Smith-Jones', '1');
 insert into subscription(id, topic, startDate, endDate, lastValidationDate, subscribingSystemIdentifier, subscriptionOwner, subjectName, active) values ('62722','{http://ojbc.org/wsn/topics}:person/incident', '2011-10-19', NULL, '2011-10-19', '{http://demostate.gov/SystemNames/1.0}SystemC', 'OJBC:IDP:OJBC:USER:admin', 'MICHAEL Smith-Jones', '1');
-insert into subscription(id, topic, startDate, endDate, lastValidationDate, subscribingSystemIdentifier, subscriptionOwner, subjectName, active) values ('62723','{http://ojbc.org/wsn/topics}:identification/rapback', '2014-10-19', '2015-10-19', '2014-10-19', '{http://demostate.gov/SystemNames/1.0}SystemC', 'OJBC:IDP:OJBC:USER:admin', 'Lisa W Simpson', '1');
-
+insert into subscription(id, topic, startDate, endDate, lastValidationDate, subscribingSystemIdentifier, subscriptionOwner, subjectName, active) values ('62723','{http://ojbc.org/wsn/topics}:person/civilArrest', '2014-10-19', '2015-10-19', '2014-10-19', '{http://demostate.gov/SystemNames/1.0}SystemC', 'OJBC:IDP:OJBC:USER:admin', 'Lisa W Simpson', '1');
+insert into subscription(id, topic, startDate, endDate, lastValidationDate, subscribingSystemIdentifier, subscriptionOwner, subjectName, active) values ('62724','{http://ojbc.org/wsn/topics}:person/civilArrest', '2014-10-19', '2015-10-19', '2014-10-19', '{http://demostate.gov/SystemNames/1.0}SystemC', 'OJBC:IDP:OJBC:USER:admin', 'Lisa W Simpson', '1');
 insert into notification_mechanism(subscriptionId,notificationMechanismType,notificationAddress) values('62721', 'email', 'email4@email.com');
 insert into notification_mechanism(subscriptionId,notificationMechanismType,notificationAddress) values('62721', 'email', 'email101@email.com');
 
@@ -30,6 +30,8 @@ insert into notification_mechanism(subscriptionId,notificationMechanismType,noti
 insert into notification_mechanism(subscriptionId,notificationMechanismType,notificationAddress) values('62723', 'email', 'email5@email.com');
 insert into notification_mechanism(subscriptionId,notificationMechanismType,notificationAddress) values('62723', 'email', 'email103@email.com');
 
+insert into notification_mechanism(subscriptionId,notificationMechanismType,notificationAddress) values('62724', 'email', 'email105@email.com');
+insert into notification_mechanism(subscriptionId,notificationMechanismType,notificationAddress) values('62724', 'email', 'email6@email.com');
 
 insert into subscription_subject_identifier(subscriptionId, identifierName, identifierValue) values('62720', 'subscriptionQualifier', '2109639');
 insert into subscription_subject_identifier(subscriptionId, identifierName, identifierValue) values('62720', 'dateOfBirth', '1960-10-02');
@@ -46,6 +48,11 @@ insert into subscription_subject_identifier(subscriptionId, identifierName, iden
 insert into subscription_subject_identifier(subscriptionId, identifierName, identifierValue) values('62723', 'firstName', 'Lisa');
 insert into subscription_subject_identifier(subscriptionId, identifierName, identifierValue) values('62723', 'lastName', 'Simpson');
 
+insert into subscription_subject_identifier(subscriptionId, identifierName, identifierValue) values('62724', 'subscriptionQualifier', '2110224');
+insert into subscription_subject_identifier(subscriptionId, identifierName, identifierValue) values('62724', 'dateOfBirth', '1990-10-12');
+insert into subscription_subject_identifier(subscriptionId, identifierName, identifierValue) values('62724', 'firstName', 'Lisa');
+insert into subscription_subject_identifier(subscriptionId, identifierName, identifierValue) values('62724', 'lastName', 'Simpson');
+
 insert into finger_prints_type(finger_prints_type_id, finger_prints_type) values('1', 'FBI'); 
 insert into finger_prints_type(finger_prints_type_id, finger_prints_type) values('2', 'State'); 
 
@@ -57,8 +64,8 @@ insert into IDENTIFICATION_SUBJECT(subject_id, ucn, civil_sid, criminal_sid, dob
 insert into IDENTIFICATION_SUBJECT(subject_id, ucn, civil_sid, criminal_sid, dob, first_name, last_name, middle_initial, sex_code) 
 	values ('2', 'B1234569','A123458', 'C1234568', '1987-10-10', 'Bart', 'Simpson', 'C','M');
 
-insert into IDENTIFICATION_TRANSACTION (TRANSACTION_NUMBER, SUBJECT_ID, OTN, OWNER_ORI, OWNER_PROGRAM_OCA, IDENTIFICATION_CATEGORY, ARCHIVED) 
-	values ('000001820140729014008339990', '1', '12343', '68796860', 'ID12345', 'I', 'false');
+insert into IDENTIFICATION_TRANSACTION (TRANSACTION_NUMBER, SUBJECT_ID, OTN, OWNER_ORI, OWNER_PROGRAM_OCA, IDENTIFICATION_CATEGORY, ARCHIVED, Subscription_ID) 
+	values ('000001820140729014008339990', '1', '12343', '68796860', 'ID12345', 'I', 'false', '62724');
 insert into IDENTIFICATION_TRANSACTION (TRANSACTION_NUMBER, SUBJECT_ID, OTN, OWNER_ORI, OWNER_PROGRAM_OCA, IDENTIFICATION_CATEGORY, ARCHIVED) 
 	values ('000001820140729014008339991', '2', '12344', '68796860', 'ID12345', 'CAR', 'false');
 insert into IDENTIFICATION_TRANSACTION (TRANSACTION_NUMBER, SUBJECT_ID, OTN, OWNER_ORI, OWNER_PROGRAM_OCA, IDENTIFICATION_CATEGORY, ARCHIVED, Subscription_ID) 
