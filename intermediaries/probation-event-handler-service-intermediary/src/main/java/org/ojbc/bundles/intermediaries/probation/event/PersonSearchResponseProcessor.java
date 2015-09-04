d5372fbef4170d0e17996d0ecd6a38578de548d5
criffle <criffle@search.org>
2015-09-04 13:03:10 -0400

    prob. intermediary - edited mock data to work with testing the full manual route

--- intermediaries/probation-event-handler-service-intermediary/src/main/java/org/ojbc/bundles/intermediaries/probation/event/PersonSearchResponseProcessor.java
+++ intermediaries/probation-event-handler-service-intermediary/src/main/java/org/ojbc/bundles/intermediaries/probation/event/PersonSearchResponseProcessor.java
@@ -16,38 +16,32 @@
  */
 package org.ojbc.bundles.intermediaries.probation.event;
 
-import java.io.InputStream;
-import java.io.StringWriter;
-import java.util.logging.Logger;
-
 import org.apache.camel.Exchange;
-import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
 import org.ojbc.util.xml.XmlUtils;
 import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 
+public class PersonSearchResponseProcessor {
+	
+	private Logger logger = Logger.getLogger(PersonSearchResponseProcessor.class);
+	
+	public void setFbiIdHeaderFromPersonSearchResponse(Exchange exchange, Document personSearchResponse) throws Exception{
+		
+		NodeList responseNodeList = XmlUtils.xPathNodeListSearch(personSearchResponse, "//psres:PersonSearchResult");
+		
+		if(responseNodeList != null && responseNodeList.getLength() == 1){
+			
+			String fbiId = XmlUtils.xPathStringSearch(personSearchResponse, 
+					"//psres:PersonSearchResult/emrm-ext:Person/jxdm41:PersonAugmentation/jxdm41:PersonFBIIdentification/nc:IdentificationID");
+			
+			logger.info("\n\n\n Processor returning fbiId:  " + fbiId + "\n\n\n");
+			
+			exchange.getIn().setHeader("fbiId", fbiId);				
+		
+		}else{			
+			logger.error("\n\n\n WARNING Person Responses NOT = 1, (NOT setting fbi id now)  \n\n\n");
+		}	
+	}	
 
-public class MockPersonSearchProcessor {
-		
-	private static final Logger logger = Logger.getLogger(MockPersonSearchProcessor.class.getName());
-	
-	
-	public String mockPersonSearch(String personSearchRequest) throws Exception{
-				
-		InputStream responseInStream = getClass().getClassLoader().getResourceAsStream("mock/Person_EntityMergeResultMessage.xml");
-		
-		if(responseInStream == null){
-			throw new Exception("responseInStream NULL!!!");
-		}				
-		
-		StringWriter responseStringWriter = new StringWriter();
-		
-		IOUtils.copy(responseInStream, responseStringWriter);
-		
-		String personResponse = responseStringWriter.toString();
-		
-		logger.info("\n\n\n Mock Processor returning: \n\n" + personResponse);
-				
-		return personResponse;
-	}
-	  			
 }
