70b1d059c2f0e837dbf7aa792cde2cf775e4e345
Yogesh Chawla <yogesh@search.org>
2015-09-28 13:21:59 -0500

    Added SAMLAssertionBuilder.  This class allows you to dynamically set keystore information that you can use to optionally sign the document.  SAMLAssertionBuilder also sets the content of the token through a method call and does not provide any defaults.  SamlTokenUtils now invokes this class instead of building an assertion and preserves the same behavior of setting default token values.  Modified all classes to use the SamlAttribute enumeration rather than hardcoding attribute names.

--- intermediaries/subscription-notification-service-intermediary/src/test/java/org/ojbc/intermediaries/sn/tests/CamelContextSearchQueryTest.java
+++ intermediaries/subscription-notification-service-intermediary/src/test/java/org/ojbc/intermediaries/sn/tests/CamelContextSearchQueryTest.java
@@ -22,6 +22,7 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -52,6 +53,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.ojbc.util.camel.helper.OJBUtils;
 import org.ojbc.util.camel.security.saml.SAMLTokenUtils;
+import org.ojbc.util.model.saml.SamlAttribute;
 import org.ojbc.util.xml.XmlUtils;
 import org.opensaml.saml2.core.Assertion;
 import org.opensaml.xml.signature.SignatureConstants;
@@ -223,9 +225,16 @@ public class CamelContextSearchQueryTest extends AbstractSubscriptionNotificatio
     }
 
 	private SAMLTokenPrincipal createSAMLToken() throws Exception {
+		
+		Map<SamlAttribute, String> customAttributes = new HashMap<SamlAttribute, String>();
+		
+		customAttributes.put(SamlAttribute.EmployerSubUnitName, "OJBC:IDP:OJBC");
+		customAttributes.put(SamlAttribute.FederationId, "OJBC:IDP:OJBC:USER:admin");
+		customAttributes.put(SamlAttribute.IdentityProviderId, "OJBC");
+		
 		Assertion samlToken = SAMLTokenUtils.createStaticAssertionWithCustomAttributes("https://idp.ojbc-local.org:9443/idp/shibboleth",
-				SignatureConstants.ALGO_ID_C14N_EXCL_OMIT_COMMENTS, SignatureConstants.ALGO_ID_SIGNATURE_RSA_SHA1, true, true, null,
-				"OJBC:IDP:OJBC", "OJBC:IDP:OJBC:USER:admin", "OJBC");
+				SignatureConstants.ALGO_ID_C14N_EXCL_OMIT_COMMENTS, SignatureConstants.ALGO_ID_SIGNATURE_RSA_SHA1, true, true, customAttributes);
+		
 		SAMLTokenPrincipal principal = new SAMLTokenPrincipal(new AssertionWrapper(samlToken));
 		return principal;
 	}
