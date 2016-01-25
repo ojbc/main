cd768acc79ddfd10fb0d2352769ec5facaf18739
Haiqi Wei <haiqi@search.org>
2016-01-25 14:08:44 -0600

    Organized imports to remove warnings.

--- intermediaries/identification-reporting-service-intermediary/src/test/java/org/ojbc/bundles/intermediaries/identificationreporting/ArrestReportTransformTest.java
+++ intermediaries/identification-reporting-service-intermediary/src/test/java/org/ojbc/bundles/intermediaries/identificationreporting/ArrestReportTransformTest.java
@@ -20,8 +20,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
