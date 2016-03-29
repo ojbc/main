9e594485eb835b4b31c4fbba9fd46e3a6f6b01f9
Haiqi Wei <haiqi@search.org>
2016-03-29 14:11:27 -0500

    Rename package name, cfg file and artifact id to reflect project name change.

---
+++ adapters/analytical-custody-data-store-persistence-adapter/src/main/java/org/ojbc/adapters/analyticaldatastore/custody/dao/IncidentRowMapper.java
@@ -0,0 +1,52 @@
+/*
+ * Unless explicitly acquired and licensed from Licensor under another license, the contents of
+ * this file are subject to the Reciprocal Public License ("RPL") Version 1.5, or subsequent
+ * versions as allowed by the RPL, and You may not copy or use this file in either source code
+ * or executable form, except in compliance with the terms and conditions of the RPL
+ *
+ * All software distributed under the RPL is provided strictly on an "AS IS" basis, WITHOUT
+ * WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND LICENSOR HEREBY DISCLAIMS ALL SUCH
+ * WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific language
+ * governing rights and limitations under the RPL.
+ *
+ * http://opensource.org/licenses/RPL-1.5
+ *
+ * Copyright 2012-2015 Open Justice Broker Consortium
+ */
+package org.ojbc.adapters.analyticaldatastore.custody.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.apache.commons.lang.StringUtils;
+import org.ojbc.adapters.analyticaldatastore.custody.dao.model.Incident;
+import org.springframework.jdbc.core.RowMapper;
+
+public class IncidentRowMapper implements RowMapper<Incident>
+{
+	@Override
+	public Incident mapRow(ResultSet rs, int rowNum) throws SQLException {
+    	Incident incident = new Incident();
+    	
+    	incident.setIncidentCaseNumber(rs.getString("IncidentCaseNumber"));
+    	incident.setIncidentDate(rs.getDate("IncidentDate"));
+    	incident.setIncidentID(rs.getInt("IncidentID"));
+    	incident.setIncidentLocationStreetAddress(rs.getString("IncidentLocationStreetAddress"));
+    	incident.setIncidentLocationTown(rs.getString("IncidentLocationTown"));
+    	incident.setIncidentTime(rs.getTime("IncidentTime"));
+    	incident.setReportingAgencyID(rs.getInt("ReportingAgencyID"));
+    	incident.setIncidentLocationLatitude(rs.getBigDecimal("IncidentLocationLatitude"));
+    	incident.setIncidentLocationLongitude(rs.getBigDecimal("IncidentLocationLongitude"));
+    	incident.setReportingSystem(rs.getString("ReportingSystem"));
+    	
+    	String recordType = rs.getString("RecordType"); 
+    	
+    	if (StringUtils.isNotBlank(recordType)){
+    		incident.setRecordType(recordType.charAt(0));
+    	}
+    	
+    	return incident;
+	}
+
+}
---
+++ adapters/analytical-custody-data-store-persistence-adapter/src/main/java/org/ojbc/adapters/analyticaldatastore/custody/dao/model/Incident.java
@@ -0,0 +1,151 @@
+/*
+ * Unless explicitly acquired and licensed from Licensor under another license, the contents of
+ * this file are subject to the Reciprocal Public License ("RPL") Version 1.5, or subsequent
+ * versions as allowed by the RPL, and You may not copy or use this file in either source code
+ * or executable form, except in compliance with the terms and conditions of the RPL
+ *
+ * All software distributed under the RPL is provided strictly on an "AS IS" basis, WITHOUT
+ * WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND LICENSOR HEREBY DISCLAIMS ALL SUCH
+ * WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific language
+ * governing rights and limitations under the RPL.
+ *
+ * http://opensource.org/licenses/RPL-1.5
+ *
+ * Copyright 2012-2015 Open Justice Broker Consortium
+ */
+package org.ojbc.adapters.analyticaldatastore.custody.dao.model;
+
+import java.math.BigDecimal;
+import java.sql.Time;
+import java.util.Date;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+
+public class Incident {
+
+	//pk
+	private Integer incidentID;
+	
+	//fk to Agency table
+	private Integer reportingAgencyID;
+		
+	private Date incidentDate;
+	private Time incidentTime;
+	
+	private String incidentCaseNumber;
+	
+	private BigDecimal incidentLocationLatitude;
+	private BigDecimal IncidentLocationLongitude;
+    private String incidentLocationStreetAddress;
+    private String incidentLocationTown;
+	
+    private String incidentDescriptionText;
+    
+	private char recordType;
+
+	private String reportingSystem;
+	
+	public Integer getIncidentID() {
+		return incidentID;
+	}
+
+	public void setIncidentID(Integer incidentID) {
+		this.incidentID = incidentID;
+	}
+
+	public Integer getReportingAgencyID() {
+		return reportingAgencyID;
+	}
+
+	public void setReportingAgencyID(Integer reportingAgencyID) {
+		this.reportingAgencyID = reportingAgencyID;
+	}
+
+	public Date getIncidentDate() {
+		return incidentDate;
+	}
+
+	public void setIncidentDate(Date incidentDate) {
+		this.incidentDate = incidentDate;
+	}
+
+	public String getIncidentCaseNumber() {
+		return incidentCaseNumber;
+	}
+
+	public void setIncidentCaseNumber(String incidentCaseNumber) {
+		this.incidentCaseNumber = incidentCaseNumber;
+	}
+
+	public BigDecimal getIncidentLocationLatitude() {
+		return incidentLocationLatitude;
+	}
+
+	public void setIncidentLocationLatitude(BigDecimal incidentLocationLatitude) {
+		this.incidentLocationLatitude = incidentLocationLatitude;
+	}
+
+	public BigDecimal getIncidentLocationLongitude() {
+		return IncidentLocationLongitude;
+	}
+
+	public void setIncidentLocationLongitude(BigDecimal incidentLocationLongitude) {
+		IncidentLocationLongitude = incidentLocationLongitude;
+	}
+
+	public String getIncidentLocationStreetAddress() {
+		return incidentLocationStreetAddress;
+	}
+
+	public void setIncidentLocationStreetAddress(
+			String incidentLocationStreetAddress) {
+		this.incidentLocationStreetAddress = incidentLocationStreetAddress;
+	}
+
+	public String getIncidentLocationTown() {
+		return incidentLocationTown;
+	}
+
+	public void setIncidentLocationTown(String incidentLocationTown) {
+		this.incidentLocationTown = incidentLocationTown;
+	}
+
+	public Time getIncidentTime() {
+		return incidentTime;
+	}
+
+	public void setIncidentTime(Time incidentTime) {
+		this.incidentTime = incidentTime;
+	}
+
+	public char getRecordType() {
+		return recordType;
+	}
+
+	public void setRecordType(char recordType) {
+		this.recordType = recordType;
+	}
+
+	public String getIncidentDescriptionText() {
+		return incidentDescriptionText;
+	}
+
+	public void setIncidentDescriptionText(String incidentDescriptionText) {
+		this.incidentDescriptionText = incidentDescriptionText;
+	}
+
+	public String getReportingSystem() {
+		return reportingSystem;
+	}
+
+	public void setReportingSystem(String reportingSystem) {
+		this.reportingSystem = reportingSystem;
+	}
+	
+	public String toString(){
+		return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+	}
+	
+}
