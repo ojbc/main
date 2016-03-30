9ea3be5ebdd309f3f42a8e9cc543e93b6b568975
Haiqi Wei <haiqi@search.org>
2016-03-30 15:24:58 -0500

    Add needed model classes and modify the Person class to have languageId foreign key.

---
+++ adapters/custody-analytics-staging-adapter/src/main/java/org/ojbc/adapters/analyticsstaging/custody/dao/model/BookingSubject.java
@@ -0,0 +1,112 @@
+/*
+ * Unless explicitly acquired and licensed from Licensor under another license, the contents of
+ * this file are subject to the Reciprocal Public License ("RPL") Version 1.5, or subsequent
+ * versions as allowed by the RPL, and You may not copy or use this file in either source code
+ * or executable form, except in compliance with the terms and conditions of the RPL
+ *
+ * All software distributed under the RPL is provided strictly on an "AS IS" basis, WITHOUT
+ * WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND LICENSOR HEREBY DISCLAIMS ALL SUCH
+ * WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific language
+ * governing rights and limitations under the RPL.
+ *
+ * http://opensource.org/licenses/RPL-1.5
+ *
+ * Copyright 2012-2015 Open Justice Broker Consortium
+ */
+package org.ojbc.adapters.analyticsstaging.custody.dao.model;
+import java.io.Serializable;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+
+public class BookingSubject implements Serializable
+{
+	private static final long serialVersionUID = 3530940339345199825L;
+	private Long bookingSubjectId;
+	private Integer recidivistIndicator; 
+	private Integer persoinId; 
+	private String bookingNumber; 
+	private Integer personAge; 
+	private Integer educationLevelId; 
+	private Integer occupationId; 
+	private Integer incomeLevelId; 
+	private Integer housingStatusId; 
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);	
+	}
+
+	public Long getBookingSubjectId() {
+		return bookingSubjectId;
+	}
+
+	public void setBookingSubjectId(Long bookingSubjectId) {
+		this.bookingSubjectId = bookingSubjectId;
+	}
+
+	public Integer getRecidivistIndicator() {
+		return recidivistIndicator;
+	}
+
+	public void setRecidivistIndicator(Integer recidivistIndicator) {
+		this.recidivistIndicator = recidivistIndicator;
+	}
+
+	public Integer getPersoinId() {
+		return persoinId;
+	}
+
+	public void setPersoinId(Integer persoinId) {
+		this.persoinId = persoinId;
+	}
+
+	public String getBookingNumber() {
+		return bookingNumber;
+	}
+
+	public void setBookingNumber(String bookingNumber) {
+		this.bookingNumber = bookingNumber;
+	}
+
+	public Integer getPersonAge() {
+		return personAge;
+	}
+
+	public void setPersonAge(Integer personAge) {
+		this.personAge = personAge;
+	}
+
+	public Integer getEducationLevelId() {
+		return educationLevelId;
+	}
+
+	public void setEducationLevelId(Integer educationLevelId) {
+		this.educationLevelId = educationLevelId;
+	}
+
+	public Integer getOccupationId() {
+		return occupationId;
+	}
+
+	public void setOccupationId(Integer occupationId) {
+		this.occupationId = occupationId;
+	}
+
+	public Integer getIncomeLevelId() {
+		return incomeLevelId;
+	}
+
+	public void setIncomeLevelId(Integer incomeLevelId) {
+		this.incomeLevelId = incomeLevelId;
+	}
+
+	public Integer getHousingStatusId() {
+		return housingStatusId;
+	}
+
+	public void setHousingStatusId(Integer housingStatusId) {
+		this.housingStatusId = housingStatusId;
+	}
+}
