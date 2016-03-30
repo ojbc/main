7ba1851093f7de56b7d78724ff6346a231e7a61e
Haiqi Wei <haiqi@search.org>
2016-03-30 13:51:44 -0500

    Set up the initial junit test to test the booking reporting route.

--- adapters/custody-analytics-staging-adapter/src/test/java/org/ojbc/adapters/analyticsstaging/custody/CamelContextTest.java
+++ adapters/custody-analytics-staging-adapter/src/test/java/org/ojbc/adapters/analyticsstaging/custody/CamelContextTest.java
@@ -16,23 +16,46 @@
  */
 package org.ojbc.adapters.analyticsstaging.custody;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import javax.annotation.Resource;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
+import javax.annotation.Resource;
+import javax.xml.namespace.QName;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.apache.camel.EndpointInject;
+import org.apache.camel.Exchange;
 import org.apache.camel.Produce;
 import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.AdviceWithRouteBuilder;
+import org.apache.camel.component.cxf.common.message.CxfConstants;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.impl.DefaultExchange;
 import org.apache.camel.model.ModelCamelContext;
 import org.apache.camel.test.spring.CamelSpringJUnit4ClassRunner;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.cxf.binding.soap.SoapHeader;
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.headers.Header;
+import org.apache.cxf.message.MessageImpl;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ojbc.adapters.analyticsstaging.custody.dao.AnalyticalDatastoreDAOImpl;
+import org.ojbc.util.camel.helper.OJBUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 @RunWith(CamelSpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
@@ -52,9 +75,12 @@ public class CamelContextTest {
     
     @Produce
     protected ProducerTemplate template;
-    
+
 	@Autowired
 	private AnalyticalDatastoreDAOImpl analyticalDatastoreDAOImpl;
+	
+	@EndpointInject(uri = "mock:direct:failedInvocation")
+	protected MockEndpoint failedInvocationEndpoint;
 	
 	@Test
 	public void contextStartup() {
@@ -63,7 +89,87 @@ public class CamelContextTest {
 
 	@Before
 	public void setUp() throws Exception {
+    	//We replace the 'from' web service endpoint with a direct endpoint we call in our test
+    	context.getRouteDefinition("Booking_Reporting_Service").adviceWith(context, new AdviceWithRouteBuilder() {
+    	    @Override
+    	    public void configure() throws Exception {
+    	    	// The line below allows us to bypass CXF and send a message directly into the route
+    	    	replaceFromWith("direct:bookingReportServiceEndpoint");
+    	    }              
+    	});
+
+    	context.getRouteDefinition("Booking_Reporting_Service_Process_Booking_Report").adviceWith(context, new AdviceWithRouteBuilder() {
+    	    @Override
+    	    public void configure() throws Exception {
+    	    	//This assists testing an invocation failure
+    	    	interceptSendToEndpoint("direct:failedInvocation").to("mock:direct:failedInvocation").stop();
+    	    }              
+    	});
+    	
+    	
     	context.start();
 	}	
 	
+	@Test
+	public void testIncidentReportService() throws Exception
+	{
+    	Exchange incidentReportExchange = createSenderExchange("src/test/resources/xmlInstances/bookingReport/BookingReportJail.xml");
+	    
+	    //Send the one-way exchange.  Using template.send will send an one way message
+		Exchange returnExchange = template.send("direct:bookingReportServiceEndpoint", incidentReportExchange);
+		
+		//Use getException to see if we received an exception
+		if (returnExchange.getException() != null)
+		{	
+			throw new Exception(returnExchange.getException());
+		}	
+
+	}
+	
+	protected Exchange createSenderExchange(String inputFilePath) throws Exception, IOException {
+		//Create a new exchange
+    	Exchange senderExchange = new DefaultExchange(context);
+
+    	//Set the WS-Address Message ID
+		Map<String, Object> requestContext = OJBUtils.setWSAddressingMessageID("123456789");
+		
+		//Set the operation name and operation namespace for the CXF exchange
+		senderExchange.getIn().setHeader(Client.REQUEST_CONTEXT , requestContext);
+		
+		Document doc = createDocument();
+		List<SoapHeader> soapHeaders = new ArrayList<SoapHeader>();
+		soapHeaders.add(makeSoapHeader(doc, "http://www.w3.org/2005/08/addressing", "MessageID", "12345"));
+		senderExchange.getIn().setHeader(Header.HEADER_LIST , soapHeaders);
+		
+		org.apache.cxf.message.Message message = new MessageImpl();
+
+		senderExchange.getIn().setHeader(CxfConstants.CAMEL_CXF_MESSAGE, message);
+    	
+	    //Read the firearm search request file from the file system
+	    File inputFile = new File(inputFilePath);
+	    String inputStr = FileUtils.readFileToString(inputFile);
+
+	    assertNotNull(inputStr);
+	    
+	    //Set it as the message message body
+	    senderExchange.getIn().setBody(inputStr);
+		return senderExchange;
+	}
+	
+	private SoapHeader makeSoapHeader(Document doc, String namespace, String localName, String value) {
+		Element messageId = doc.createElementNS(namespace, localName);
+		messageId.setTextContent(value);
+		SoapHeader soapHeader = new SoapHeader(new QName(namespace, localName), messageId);
+		return soapHeader;
+	}	
+	
+	public static Document createDocument() throws Exception{
+
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		dbf.setNamespaceAware(true);
+		Document doc = dbf.newDocumentBuilder().newDocument();
+
+		return doc;
+	}
+
 }
