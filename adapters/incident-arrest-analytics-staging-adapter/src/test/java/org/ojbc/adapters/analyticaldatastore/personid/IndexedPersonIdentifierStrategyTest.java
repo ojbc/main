a7a9754c10e580226582364dafb7cffa884073b8
Haiqi Wei <haiqi@search.org>
2016-04-07 13:25:55 -0500

    Add License text.

--- adapters/incident-arrest-analytics-staging-adapter/src/test/java/org/ojbc/adapters/analyticaldatastore/personid/IndexedPersonIdentifierStrategyTest.java
+++ adapters/incident-arrest-analytics-staging-adapter/src/test/java/org/ojbc/adapters/analyticaldatastore/personid/IndexedPersonIdentifierStrategyTest.java
@@ -1,3 +1,19 @@
+/*
+ * Unless explicitly acquired and licensed from Licensor under another license, the contents of
+ * this file are subject to the Reciprocal Public License ("RPL") Version 1.5, or subsequent
+ * versions as allowed by the RPL, and You may not copy or use this file in either source code
+ * or executable form, except in compliance with the terms and conditions of the RPL
+ *
+ * All software distributed under the RPL is provided strictly on an "AS IS" basis, WITHOUT
+ * WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND LICENSOR HEREBY DISCLAIMS ALL SUCH
+ * WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific language
+ * governing rights and limitations under the RPL.
+ *
+ * http://opensource.org/licenses/RPL-1.5
+ *
+ * Copyright 2012-2015 Open Justice Broker Consortium
+ */
 package org.ojbc.adapters.analyticaldatastore.personid;
 
 import static org.junit.Assert.assertEquals;
@@ -13,7 +29,6 @@ import org.apache.commons.io.FileUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.ojbc.util.lucene.personid.IdentifierGenerationStrategy;
 import org.ojbc.util.lucene.personid.IndexedIdentifierGenerationStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
