db31f5d2c82abfd747e17206e9bac8ff3e7c709c
Haiqi Wei <haiqi@search.org>
2016-05-04 11:55:54 -0500

    Change the invokeAccessControlRequest method impl and createPolicyBasedAccessControlRequest method to take string array of requested resource URIs.

--- connectors/ojb-web-application-connector/src/main/java/org/ojbc/processor/policy/query/IdentityBasedAccessControlRequestProcessor.java
+++ connectors/ojb-web-application-connector/src/main/java/org/ojbc/processor/policy/query/IdentityBasedAccessControlRequestProcessor.java
@@ -51,7 +51,7 @@ public class IdentityBasedAccessControlRequestProcessor extends RequestResponseP
 
     private final Log log = LogFactory.getLog(this.getClass());
     
-    public String invokeAccessControlRequest(String federatedQueryID, Element samlToken, String requestedResourceURI){
+    public String invokeAccessControlRequest(String federatedQueryID, Element samlToken, String... requestedResourceURIs){
         String response = null;
         try {
             if (allowQueriesWithoutSAMLToken) {
@@ -71,7 +71,7 @@ public class IdentityBasedAccessControlRequestProcessor extends RequestResponseP
 
             // POJO to XML Request
             Document policyBasedAccessControlRequest = RequestMessageBuilderUtilities
-                    .createPolicyBasedAccessControlRequest(samlToken, requestedResourceURI);
+                    .createPolicyBasedAccessControlRequest(samlToken, requestedResourceURIs);
             policyBasedAccessControlRequest.normalizeDocument();
 
             // Create exchange
