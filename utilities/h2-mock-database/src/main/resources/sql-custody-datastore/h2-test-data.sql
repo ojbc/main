be25fbf67ffd167a72c5e878381426a74e719d2f
criffle <criffle@search.org>
2016-05-23 15:52:20 -0600

    h2 utils: updated custody db to allow more nulls and added more test data

--- utilities/h2-mock-database/src/main/resources/sql-custody-datastore/h2-test-data.sql
+++ utilities/h2-mock-database/src/main/resources/sql-custody-datastore/h2-test-data.sql
@@ -17,3 +17,6 @@
 
 INSERT INTO person(unique_person_id, sex_offender, education, primary_language, dob, ethnicity, race, sid, first_name, middle_name, last_name, sex, occupation, military_service) VALUES
 ('123', false, 'phd', 'francais', date '2000-01-01', 'a', 'a', '123', 'homer', 'jay', 'simpson', 'm', 'software engineer', false);
+
+INSERT INTO booking(id, person_id, booking_number, booking_date, facility, booking_photo, actual_release_datetime, commit_date, scheduled_release_date, block, bed, cell, case_status, inmate_work_release_indicator, inmate_worker_indicator) values
+(1, null, 1234, date '2000-01-01', 'county jail', 'profile.jpg', date '2000-01-01', date '2000-01-01', date '2000-01-01', 'a', '23', '7', '1', true, true);
