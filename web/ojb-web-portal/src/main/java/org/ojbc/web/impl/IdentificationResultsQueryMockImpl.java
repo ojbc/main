cbee56b029be7d6c283913e6cdcdfe1f4cace88d
Haiqi Wei <haiqi@search.org>
2016-06-09 15:15:20 -0500

    Modify rapback search and criminal identification search to send a default search request with configurable date range and status other than archived.

--- web/ojb-web-portal/src/main/java/org/ojbc/web/impl/IdentificationResultsQueryMockImpl.java
+++ web/ojb-web-portal/src/main/java/org/ojbc/web/impl/IdentificationResultsQueryMockImpl.java
@@ -22,7 +22,7 @@ import java.util.logging.Logger;
 import javax.annotation.Resource;
 
 import org.ojbc.web.IdentificationResultsQueryInterface;
-import org.ojbc.web.model.IdentificationResultsQueryResponse;
+import org.ojbc.web.model.identificationresult.search.IdentificationResultsQueryResponse;
 import org.ojbc.web.portal.services.SearchResultConverter;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
