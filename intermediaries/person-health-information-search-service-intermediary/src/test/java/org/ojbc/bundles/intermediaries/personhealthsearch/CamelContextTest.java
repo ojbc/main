294717ee464bd7ea60408db959c968865275dcd9
criffle <criffle@search.org>
2016-07-18 17:49:00 -0400

    person health intermediary: added a minimal camel test for the response route

--- intermediaries/person-health-information-search-service-intermediary/src/test/java/org/ojbc/bundles/intermediaries/personhealthsearch/CamelContextTest.java
+++ intermediaries/person-health-information-search-service-intermediary/src/test/java/org/ojbc/bundles/intermediaries/personhealthsearch/CamelContextTest.java
@@ -66,10 +66,11 @@ import org.w3c.dom.Element;
 		"classpath:META-INF/spring/properties-context.xml"})
 public class CamelContextTest {
 		
-	public static final String CXF_OPERATION_NAME = "SubmitPersonHealthInformationSearchRequest";
+	public static final String CXF_OPERATION_NAME = 
+			"SubmitPersonHealthInformationSearchResults";
 	
 	public static final String CXF_OPERATION_NAMESPACE = 
-			"http://ojbc.org/Services/WSDL/PersonHealthInformationSearchRequestService/1.0";
+			"http://ojbc.org/Services/WSDL/PersonHealthInformationSearchResultsService/1.0";
 	
 	private static final Log log = LogFactory.getLog( CamelContextTest.class );
 	
@@ -86,6 +87,9 @@ public class CamelContextTest {
     @EndpointInject(uri = "mock:personHealthResponseMock")
     protected MockEndpoint personHealthResponseMockEndpoint;    
             
+    @EndpointInject(uri = "mock:personHealthClientResponseMock")
+    protected MockEndpoint personHealthServiceClientResultsMockEndpoint;
+    
     @Test
     public void testApplicationStartup() {
     	assertTrue(true);
@@ -112,7 +116,8 @@ public class CamelContextTest {
     	    	// bypass CXF and send a message directly into the route
     	    	replaceFromWith("direct:personHealthResponseRouteMockEntry");    	    	
     	    	
-    	    	interceptSendToEndpoint("direct:aggregatePersonHealthResponse").to("mock:personHealthResponseMock").stop();    	
+    	    	interceptSendToEndpoint("direct:aggregatePersonHealthResponse")
+    	    		.to("mock:personHealthClientResponseMock").stop();    	
     	    }              
     	});
     	
@@ -124,6 +129,7 @@ public class CamelContextTest {
     public void testPersonHealthSearchRequest() throws Exception {
         	
     	//We should get one message
+    	personHealthRequestAdapterMockEndpoint.reset();
     	personHealthRequestAdapterMockEndpoint.expectedMessageCount(1);
 
     	//Create a new exchange
@@ -151,7 +157,7 @@ public class CamelContextTest {
 	    File inputFile = new File("src/test/resources/xmlInstances/PersonHealthInformationSearchRequest-PII.xml");
 	    String inputStr = FileUtils.readFileToString(inputFile);
 	    
-	    //Set it as the message message body
+	    //Set it as the message body
 	    senderExchange.getIn().setBody(inputStr);
 	    
 	    //Send the one-way exchange.  Using template.send will send an one way message
@@ -163,7 +169,7 @@ public class CamelContextTest {
 		}	
 		
 		//Sleep while a response is generated
-		Thread.sleep(3000);
+//		Thread.sleep(3000);
 		
 		//Assert that the mock endpoint expectations are satisfied
 		personHealthRequestAdapterMockEndpoint.assertIsSatisfied();
@@ -182,6 +188,72 @@ public class CamelContextTest {
 		
 		XmlUtils.printNode(actualResponseDocument);
     }	
+    
+    
+    @Test
+    public void testPersonHealthSearchResults() throws Exception {
+        		    	    	
+    	//We should get one message
+    	personHealthServiceClientResultsMockEndpoint.reset();
+        personHealthServiceClientResultsMockEndpoint.expectedMessageCount(1);
+
+    	//Create a new exchange
+    	Exchange senderExchange = new DefaultExchange(context);
+				
+    	//Set the WS-Address Message ID
+		Map<String, Object> requestContext = OJBUtils.setWSAddressingMessageID("12345");
+		
+		//Set the operation name and operation namespace for the CXF exchange
+		senderExchange.getIn().setHeader(Client.REQUEST_CONTEXT , requestContext);
+		
+		Document doc = OJBCXMLUtils.createDocument();
+		List<SoapHeader> soapHeaders = new ArrayList<SoapHeader>();
+		soapHeaders.add(createSoapHeader(doc, "http://www.w3.org/2005/08/addressing", "MessageID", "12345"));
+		soapHeaders.add(createSoapHeader(doc, "http://www.w3.org/2005/08/addressing", "ReplyTo", 
+				"https://localhost:8443/OJB/connector/PersonHealthSearchResultsHandlerService"));
+		senderExchange.getIn().setHeader(Header.HEADER_LIST , soapHeaders);
+		
+		org.apache.cxf.message.Message message = new MessageImpl();
+		
+		senderExchange.getIn().setHeader(CxfConstants.CAMEL_CXF_MESSAGE, message);
+ 	    senderExchange.getIn().setHeader(CxfConstants.OPERATION_NAME, CXF_OPERATION_NAME);
+	    senderExchange.getIn().setHeader(CxfConstants.OPERATION_NAMESPACE, CXF_OPERATION_NAMESPACE);
+	    
+	    File inputFile = new File("src/test/resources/xmlInstances/PersonHealthInformationSearchResults-full-detail.xml");
+	    String inputStr = FileUtils.readFileToString(inputFile);
+	    
+	    //Set it as the message body
+	    senderExchange.getIn().setBody(inputStr);
+	    
+	    //Send the one-way exchange.  Using template.send will send an one way message
+		Exchange returnExchange = template.send("direct:personHealthResponseRouteMockEntry", senderExchange);
+		
+		//Use getException to see if we received an exception
+		if (returnExchange.getException() != null){	
+			throw new Exception(returnExchange.getException());
+		}	
+		
+		//Sleep while a response is generated
+//		Thread.sleep(3000);
+		
+		//Assert that the mock endpoint expectations are satisfied
+		personHealthServiceClientResultsMockEndpoint.assertIsSatisfied();
+		
+		//Get the first exchange (the only one)
+		Exchange ex = personHealthServiceClientResultsMockEndpoint.getExchanges().get(0);
+		
+		String opName = (String)ex.getIn().getHeader("operationName");
+		assertEquals("SubmitPersonHealthInformationSearchResults", opName);
+		
+		String opNamespace = (String)ex.getIn().getHeader("operationNamespace");
+		assertEquals("http://ojbc.org/Services/WSDL/PersonHealthInformationSearchResultsService/1.0", opNamespace);
+		
+		//Get the actual response
+		Document actualResponseDocument = (Document)ex.getIn().getBody(Document.class);
+		
+		XmlUtils.printNode(actualResponseDocument);
+    }	    
+    
     
 	private SoapHeader createSoapHeader(Document doc, String namespace, String localName, String value) {
 		
