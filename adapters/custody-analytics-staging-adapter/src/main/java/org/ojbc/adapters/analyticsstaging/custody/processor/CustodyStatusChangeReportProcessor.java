fb5b567cec96f4b92d7a8c137f77b9a764202228
Haiqi Wei <haiqi@search.org>
2016-07-27 14:31:37 -0500

    Fix the header.

--- adapters/custody-analytics-staging-adapter/src/main/java/org/ojbc/adapters/analyticsstaging/custody/processor/CustodyStatusChangeReportProcessor.java
+++ adapters/custody-analytics-staging-adapter/src/main/java/org/ojbc/adapters/analyticsstaging/custody/processor/CustodyStatusChangeReportProcessor.java
@@ -1,5 +1,4 @@
 /*
-
  * Unless explicitly acquired and licensed from Licensor under another license, the contents of
  * this file are subject to the Reciprocal Public License ("RPL") Version 1.5, or subsequent
  * versions as allowed by the RPL, and You may not copy or use this file in either source code
