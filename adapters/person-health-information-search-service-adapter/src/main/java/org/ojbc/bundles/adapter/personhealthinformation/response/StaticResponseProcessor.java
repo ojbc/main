0753f3cb0b90a6fea23c75d7057243251008bfab
Yogesh Chawla <yogesh@search.org>
2016-08-22 17:59:52 -0500

    Person health information adapter that allows for a configurable camel endpoint that provides that the actual search response from a search request.

---
+++ adapters/person-health-information-search-service-adapter/src/main/java/org/ojbc/bundles/adapter/personhealthinformation/response/StaticResponseProcessor.java
@@ -0,0 +1,40 @@
+/*
+ * Unless explicitly acquired and licensed from Licensor under another license, the contents of
+ * this file are subject to the Reciprocal Public License ("RPL") Version 1.5, or subsequent
+ * versions as allowed by the RPL, and You may not copy or use this file in either source code
+ * or executable form, except in compliance with the terms and conditions of the RPL
+ *
+ * All software distributed under the RPL is provided strictly on an "AS IS" basis, WITHOUT
+ * WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND LICENSOR HEREBY DISCLAIMS ALL SUCH
+ * WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific language
+ * governing rights and limitations under the RPL.
+ *
+ * http://opensource.org/licenses/RPL-1.5
+ *
+ * Copyright 2012-2015 Open Justice Broker Consortium
+ */
+package org.ojbc.bundles.adapter.personhealthinformation.response;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class StaticResponseProcessor {
+
+	private static final Log log = LogFactory.getLog( StaticResponseProcessor.class );
+	
+	public String returnStaticResponse() throws Exception
+	{
+        String filePath = "src/main/resources/xmlInstances/PersonHealthInformationSearchResults-full-detail.xml";
+        
+        String contents = FileUtils.readFileToString(new File(filePath), "UTF-8");
+		
+        log.info("Static file contents: " + contents);
+        
+        return contents;
+	}
+	
+}
