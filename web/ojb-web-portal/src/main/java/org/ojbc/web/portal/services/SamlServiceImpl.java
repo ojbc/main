b7c02a732114984b30bc5724802d015ff361c44d
Haiqi Wei <haiqi@search.org>
2016-09-09 15:42:28 -0500

    Give the dev testing user the FirearmsRegistrationRecordsPersonnel and the SupervisoryRole privileges.

--- web/ojb-web-portal/src/main/java/org/ojbc/web/portal/services/SamlServiceImpl.java
+++ web/ojb-web-portal/src/main/java/org/ojbc/web/portal/services/SamlServiceImpl.java
@@ -138,6 +138,8 @@ public class SamlServiceImpl implements SamlService{
             customAttributes.put(SamlAttribute.FederationId, "HIJIS:IDP:HCJDC:USER:demouser");
 //                customAttributes.put(SamlAttribute.FederationId.getAttibuteName(), "HIJIS:IDP:HCJDC:USER:demouser4");
             customAttributes.put(SamlAttribute.EmployerORI, "1234567890");
+            customAttributes.put(SamlAttribute.FirearmsRegistrationRecordsPersonnelIndicator, "true");
+            customAttributes.put(SamlAttribute.SupervisoryRoleIndicator, "true");
 //            customAttributes.put(SamlAttribute.FederatedQueryUserIndicator, "");
 //                customAttributes.put("gfipm:2.0:user:EmployerORI", "H00000001");
             
