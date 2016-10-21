36a9ca54f1c2a3b4ce39bfb56ea21f37e004c245
Haiqi Wei <haiqi@search.org>
2016-10-21 14:23:16 -0500

    Organized imports.

--- intermediaries/policy-acknowledgement-service-intermediary/src/main/java/org/ojbc/policyacknowledgement/dao/PolicyDAOImpl.java
+++ intermediaries/policy-acknowledgement-service-intermediary/src/main/java/org/ojbc/policyacknowledgement/dao/PolicyDAOImpl.java
@@ -25,7 +25,6 @@ import java.util.Map;
 
 import javax.sql.DataSource;
 
-import org.apache.commons.lang.BooleanUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
