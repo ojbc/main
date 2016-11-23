4d5acf80e0a9be1935200cf37b214e765ca86ad8
criffle <criffle@search.org>
2016-11-23 17:02:17 -0500

    h2-mock-database: updated custody data to assist with testing

--- utilities/h2-mock-database/src/main/resources/sql-custody-datastore/h2-test-data.sql
+++ utilities/h2-mock-database/src/main/resources/sql-custody-datastore/h2-test-data.sql
@@ -30,7 +30,7 @@ INSERT INTO charge(arrest_id, bond_amount, bond_type, bond_status, next_court_ev
 (1, 499.0000, 'money', 'paid', 'judge judy', DATE '2070-01-01', 456, 'speeding', 'ordinance', 'driving', 'Walker Texas Ranger', 'matlock');
 
 INSERT INTO person_alias(person_id, name_type, alias_last_name, alias_first_name, alias_middle, alias_sex, alias_dob) VALUES
-(1, 'screen name', 'Simpson', 'homy', 'jay', 'M', date '2000-01-01');
+(1, 'screen name', 'Simpser', 'homy', 'jay', 'M', date '2000-01-01');
 
 INSERT INTO scars_marks_tattoos(person_id, scars_marks_tattoos_description) VALUES
 (1, 'dragon tatoo');
