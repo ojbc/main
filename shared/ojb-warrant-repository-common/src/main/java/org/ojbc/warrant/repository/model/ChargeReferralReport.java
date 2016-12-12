61bd00fa512c5f638c23dd45609b4041a3bbabf2
Yogesh Chawla <yogesh@search.org>
2016-12-12 14:43:54 -0600

    Updated to use common-lang instead of commons-lang3 due to karaf issues.

--- shared/ojb-warrant-repository-common/src/main/java/org/ojbc/warrant/repository/model/ChargeReferralReport.java
+++ shared/ojb-warrant-repository-common/src/main/java/org/ojbc/warrant/repository/model/ChargeReferralReport.java
@@ -16,10 +16,8 @@
  */
 package org.ojbc.warrant.repository.model;
 
-import java.util.List;
-
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
 
 public class ChargeReferralReport {
 
--- shared/ojb-warrant-repository-common/src/main/java/org/ojbc/warrant/repository/model/WarrantChargeReferralAssociation.java
+++ shared/ojb-warrant-repository-common/src/main/java/org/ojbc/warrant/repository/model/WarrantChargeReferralAssociation.java
@@ -16,8 +16,8 @@
  */
 package org.ojbc.warrant.repository.model;
 
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
 
 public class WarrantChargeReferralAssociation {
 
