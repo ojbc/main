5eccefa45fd60b4f896448602ca9d3d227c603ac
Haiqi Wei <haiqi@search.org>
2016-12-13 07:58:13 -0600

    Adjust to the StringUtils package change.

--- connectors/warrant-modification-service-connector/src/main/java/org/ojbc/connectors/warrantmod/dao/WarrantsRepositoryBaseDaoImpl.java
+++ connectors/warrant-modification-service-connector/src/main/java/org/ojbc/connectors/warrantmod/dao/WarrantsRepositoryBaseDaoImpl.java
@@ -26,7 +26,7 @@ import java.util.Map;
 import javax.sql.DataSource;
 
 import org.apache.camel.Header;
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.ojbc.util.helper.DaoUtils;
