90702ccb2fc30a465f1d4f995403dd8f5a108c1e
Yogesh Chawla <yogesh@search.org>
2017-01-24 14:17:29 -0600

    Added warrant cancellation model to common bundle.

---
+++ shared/ojb-warrant-repository-common/src/main/java/org/ojbc/warrant/repository/model/WarrantCancellationReport.java
@@ -0,0 +1,57 @@
+/*
+ * Unless explicitly acquired and licensed from Licensor under another license, the contents of
+ * this file are subject to the Reciprocal Public License ("RPL") Version 1.5, or subsequent
+ * versions as allowed by the RPL, and You may not copy or use this file in either source code
+ * or executable form, except in compliance with the terms and conditions of the RPL
+ *
+ * All software distributed under the RPL is provided strictly on an "AS IS" basis, WITHOUT
+ * WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND LICENSOR HEREBY DISCLAIMS ALL SUCH
+ * WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific language
+ * governing rights and limitations under the RPL.
+ *
+ * http://opensource.org/licenses/RPL-1.5
+ *
+ * Copyright 2012-2015 Open Justice Broker Consortium
+ */
+package org.ojbc.warrant.repository.model;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+
+public class WarrantCancellationReport {
+
+	private String stateWarrantRepositoryID;
+	private String operator;
+	private String warrantStatus;
+	
+	@Override
+	public String toString(){
+		return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+	}
+
+	public String getStateWarrantRepositoryID() {
+		return stateWarrantRepositoryID;
+	}
+
+	public void setStateWarrantRepositoryID(String stateWarrantRepositoryID) {
+		this.stateWarrantRepositoryID = stateWarrantRepositoryID;
+	}
+
+	public String getOperator() {
+		return operator;
+	}
+
+	public void setOperator(String operator) {
+		this.operator = operator;
+	}
+
+	public String getWarrantStatus() {
+		return warrantStatus;
+	}
+
+	public void setWarrantStatus(String warrantStatus) {
+		this.warrantStatus = warrantStatus;
+	}
+
+}
