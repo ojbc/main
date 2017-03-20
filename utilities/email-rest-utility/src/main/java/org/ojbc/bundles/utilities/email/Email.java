5ac975509a2b839b46b023c0f7159652b186dedc
Yogesh Chawla <yogesh@search.org>
2017-03-20 16:15:04 -0500

    Simple Email utility that will send email based on JSON data sent in a REST post.

---
+++ utilities/email-rest-utility/src/main/java/org/ojbc/bundles/utilities/email/Email.java
@@ -0,0 +1,49 @@
+/*
+ * Unless explicitly acquired and licensed from Licensor under another license, the contents of
+ * this file are subject to the Reciprocal Public License ("RPL") Version 1.5, or subsequent
+ * versions as allowed by the RPL, and You may not copy or use this file in either source code
+ * or executable form, except in compliance with the terms and conditions of the RPL
+ *
+ * All software distributed under the RPL is provided strictly on an "AS IS" basis, WITHOUT
+ * WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND LICENSOR HEREBY DISCLAIMS ALL SUCH
+ * WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific language
+ * governing rights and limitations under the RPL.
+ *
+ * http://opensource.org/licenses/RPL-1.5
+ *
+ * Copyright 2012-2015 Open Justice Broker Consortium
+ */
+package org.ojbc.bundles.utilities.email;
+
+public class Email {
+    private String to;
+    private String subject;
+    private String body;
+    
+	public String getTo() {
+		return to;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public String getBody() {
+		return body;
+	}
+	
+	public void setTo(String to) {
+		this.to = to;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	@Override
+	public String toString() {
+		return "Email [to=" + to + ", subject=" + subject + ", body=" + body
+				+ "]";
+	}
+}
---
+++ utilities/email-rest-utility/src/main/java/org/ojbc/bundles/utilities/email/EmailRestImpl.java
@@ -0,0 +1,63 @@
+/*
+ * Unless explicitly acquired and licensed from Licensor under another license, the contents of
+ * this file are subject to the Reciprocal Public License ("RPL") Version 1.5, or subsequent
+ * versions as allowed by the RPL, and You may not copy or use this file in either source code
+ * or executable form, except in compliance with the terms and conditions of the RPL
+ *
+ * All software distributed under the RPL is provided strictly on an "AS IS" basis, WITHOUT
+ * WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND LICENSOR HEREBY DISCLAIMS ALL SUCH
+ * WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific language
+ * governing rights and limitations under the RPL.
+ *
+ * http://opensource.org/licenses/RPL-1.5
+ *
+ * Copyright 2012-2015 Open Justice Broker Consortium
+ */
+package org.ojbc.bundles.utilities.email;
+
+import javax.annotation.Resource;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailRestImpl implements EmailInterface {
+
+	@Resource (name="${ojbcMailSenderBean:mockMailSender}")
+	JavaMailSender ojbcMailSender;
+
+	public Response sendEmail(Email email) {
+
+        SimpleMailMessage emailToSend = new SimpleMailMessage();
+        
+        if (StringUtils.isBlank(email.getTo()) || StringUtils.isBlank(email.getSubject()) ||  StringUtils.isBlank(email.getBody()))
+        {
+        	return Response.status(Status.BAD_REQUEST).entity(email).build();
+        }	
+        
+        if (StringUtils.isNotBlank(email.getTo()))
+        {	
+        	emailToSend.setTo(email.getTo());
+        }
+        
+        if (StringUtils.isNotBlank(email.getSubject()))
+        {	
+        	emailToSend.setSubject(email.getSubject());
+        }
+
+        if (StringUtils.isNotBlank(email.getBody()))
+        {	
+        	emailToSend.setText(email.getBody());
+        }
+
+        // sends the e-mail
+        ojbcMailSender.send(emailToSend);
+
+        return Response.status(Status.OK).entity(email).build();
+	}
+}
