eaa89c650aa0d960922396d935a1842f2d119b38
Yogesh Chawla <yogesh@search.org>
2017-04-19 17:28:34 -0500

    Initial check-in of charge referral service adapter.  Note that is a WAR not an OSGi bundle at the request of a user.

---
+++ adapters/charge-referral-reporting-service-adapter/src/test/java/org/ojbc/bundles/adapters/chargereferralreporting/TestChargeReferralMessageProcessor.java
@@ -0,0 +1,50 @@
+/*
+ * Unless explicitly acquired and licensed from Licensor under another license, the contents of
+ * this file are subject to the Reciprocal Public License ("RPL") Version 1.5, or subsequent
+ * versions as allowed by the RPL, and You may not copy or use this file in either source code
+ * or executable form, except in compliance with the terms and conditions of the RPL
+ *
+ * All software distributed under the RPL is provided strictly on an "AS IS" basis, WITHOUT
+ * WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND LICENSOR HEREBY DISCLAIMS ALL SUCH
+ * WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific language
+ * governing rights and limitations under the RPL.
+ *
+ * http://opensource.org/licenses/RPL-1.5
+ *
+ * Copyright 2012-2015 Open Justice Broker Consortium
+ */
+package org.ojbc.bundles.adapters.chargereferralreporting;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.ojbc.bundles.adapters.chargereferralreporting.model.ChargeReferral;
+import org.ojbc.util.camel.helper.OJBUtils;
+import org.w3c.dom.Document;
+
+public class TestChargeReferralMessageProcessor {
+
+	private Document report;
+	ChargeReferralMessageProcessor chargeReferralMessageProcessor = new ChargeReferralMessageProcessor();
+	
+	@Test
+	public void testProcessMessage() throws Exception
+	{
+
+		File inputFile = new File("src/test/resources/xml/Michigan_Charge_Referral_Report.xml");
+	    String inputStr = FileUtils.readFileToString(inputFile);
+		
+		report = OJBUtils.loadXMLFromString(inputStr);
+		assertNotNull(report);	
+		
+		ChargeReferral chargeReferral = chargeReferralMessageProcessor.processMessage(report);
+		
+		assertEquals("Incident Reporting Agency", chargeReferral.getOrganizationName());
+		
+	}
+	
+}
