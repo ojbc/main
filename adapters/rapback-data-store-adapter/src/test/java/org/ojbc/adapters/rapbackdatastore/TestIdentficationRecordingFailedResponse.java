d07b6e61719554d35eb11485a1e7cf2635b536ba
Haiqi Wei <haiqi@search.org>
2017-05-04 11:17:05 -0500

    Use camel endpoint with "synchronous=true" option to send identification response files to be able to handle the fault.

---
+++ adapters/rapback-data-store-adapter/src/test/java/org/ojbc/adapters/rapbackdatastore/TestIdentficationRecordingFailedResponse.java
@@ -0,0 +1,145 @@
+/*
+ * Unless explicitly acquired and licensed from Licensor under another license, the contents of
+ * this file are subject to the Reciprocal Public License ("RPL") Version 1.5, or subsequent
+ * versions as allowed by the RPL, and You may not copy or use this file in either source code
+ * or executable form, except in compliance with the terms and conditions of the RPL
+ *
+ * All software distributed under the RPL is provided strictly on an "AS IS" basis, WITHOUT
+ * WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND LICENSOR HEREBY DISCLAIMS ALL SUCH
+ * WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific language
+ * governing rights and limitations under the RPL.
+ *
+ * http://opensource.org/licenses/RPL-1.5
+ *
+ * Copyright 2012-2015 Open Justice Broker Consortium
+ */
+package org.ojbc.adapters.rapbackdatastore;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.annotation.Resource;
+
+import org.apache.camel.EndpointInject;
+import org.apache.camel.Exchange;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.AdviceWithRouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.model.ModelCamelContext;
+import org.apache.camel.test.spring.CamelSpringJUnit4ClassRunner;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ojbc.adapters.rapbackdatastore.dao.RapbackDAO;
+import org.ojbc.adapters.rapbackdatastore.dao.model.IdentificationTransaction;
+import org.ojbc.adapters.rapbackdatastore.processor.IdentificationRequestReportProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.xml.sax.SAXException;
+
+@RunWith(CamelSpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+        "classpath:META-INF/spring/camel-context.xml",
+        "classpath:META-INF/spring/spring-context.xml",
+        "classpath:META-INF/spring/cxf-endpoints.xml",      
+        "classpath:META-INF/spring/properties-context.xml",
+        "classpath:META-INF/spring/dao.xml",
+        "classpath:META-INF/spring/h2-mock-database-application-context.xml",
+        "classpath:META-INF/spring/h2-mock-database-context-rapback-datastore.xml",
+      })
+@DirtiesContext
+public class TestIdentficationRecordingFailedResponse {
+	
+	@SuppressWarnings("unused")
+	private static final Log log = LogFactory.getLog( TestIdentficationRecordingFailedResponse.class );
+
+	private static final String TRANSACTION_NUMBER = "000001820140729014008340000";
+	
+	@Autowired
+	RapbackDAO rapbackDAO;
+
+	@Autowired
+	IdentificationRequestReportProcessor identificationRequestReportProcessor;
+	
+    @Resource
+    private ModelCamelContext context;
+    
+    @Produce
+    protected ProducerTemplate template;
+    
+	@EndpointInject(uri = "mock:failedInvocation")
+    protected MockEndpoint failedInvocationEndpoint;
+	
+	@Value("${rapbackDatastoreAdapter.IdentificationRecordingInputDirectory}")
+	private String failedFolderDirectory; 
+	@Test
+	public void contextStartup() {
+		assertTrue(true);
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		
+		assertNotNull(identificationRequestReportProcessor);
+
+    	//We replace the 'from' web service endpoint with a direct endpoint we call in our test
+    	context.getRouteDefinition("identification_recording_service").adviceWith(context, new AdviceWithRouteBuilder() {
+    	    @Override
+    	    public void configure() throws Exception {
+    	    	// The line below allows us to bypass CXF and send a message directly into the route
+    	    	replaceFromWith("direct:identificationRecordingServiceEndpoint");
+    	    }              
+    	});
+
+    	context.start();
+	}	
+	
+	
+	@Test
+	@DirtiesContext
+	public void testIdentificationRecordingRequestAndResultsSuccess() throws Exception
+	{
+		civilRecordingRequestSuccessResponseSentToFailedFolder(); 
+	}
+
+	private void civilRecordingRequestSuccessResponseSentToFailedFolder() throws Exception, IOException,
+			InterruptedException, SAXException {
+		
+		File failedResponseFolder = new File (failedFolderDirectory + "/failed/identificationResponse");
+		FileUtils.cleanDirectory(new File (failedFolderDirectory + "/failed/"));
+		
+		IdentificationTransaction identificationTransaction = rapbackDAO.getIdentificationTransaction(TRANSACTION_NUMBER); 
+		assertNull(identificationTransaction);
+		
+		Exchange senderExchange = MessageUtils.createSenderExchange(context, 
+				"src/test/resources/xmlInstances/identificationReport/person_identification_request_state_civil.xml");
+		
+		senderExchange.getIn().setHeader("operationName", "RecordPersonFederalIdentificationRequest");
+		
+		//Send the one-way exchange.  Using template.send will send an one way message
+		Exchange returnExchange = template.send("direct:identificationRecordingServiceEndpoint", senderExchange);
+		
+		//Use getException to see if we received an exception
+		if (returnExchange.getException() != null)
+		{	
+			throw new Exception(returnExchange.getException());
+		}
+		
+		
+		assertEquals(FileUtils.listFiles(failedResponseFolder, null, false).size(), Long.valueOf(1).longValue());
+		
+	}
+	
+}
