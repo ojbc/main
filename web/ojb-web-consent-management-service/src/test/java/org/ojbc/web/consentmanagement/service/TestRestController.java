d0023df76f793b1b439befbe2a01f809e434df5e
Scott Came <scott@search.org>
2017-05-31 19:08:37 -0700

    Added initial version of consent management rest service

---
+++ web/ojb-web-consent-management-service/src/test/java/org/ojbc/web/consentmanagement/service/TestRestController.java
@@ -0,0 +1,77 @@
+/*
+ * Unless explicitly acquired and licensed from Licensor under another license, the contents of
+ * this file are subject to the Reciprocal Public License ("RPL") Version 1.5, or subsequent
+ * versions as allowed by the RPL, and You may not copy or use this file in either source code
+ * or executable form, except in compliance with the terms and conditions of the RPL
+ *
+ * All software distributed under the RPL is provided strictly on an "AS IS" basis, WITHOUT
+ * WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND LICENSOR HEREBY DISCLAIMS ALL SUCH
+ * WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific language
+ * governing rights and limitations under the RPL.
+ *
+ * http://opensource.org/licenses/RPL-1.5
+ *
+ * Copyright 2012-2015 Open Justice Broker Consortium
+ */
+package org.ojbc.web.consentmanagement.service;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class TestRestController {
+	
+	private static final String CONSENT_SEARCH_URI = "http://localhost:8080/cm-api/search";
+	
+	private final Log log = LogFactory.getLog(TestRestController.class);
+	
+    private HttpClient httpClient;
+    
+    @Before
+    public void setUp() throws Exception {
+    	RequestConfig requestConfig = RequestConfig.custom().build();
+    	HttpClientBuilder clientBuilder = HttpClientBuilder.create();
+    	clientBuilder.setDefaultRequestConfig(requestConfig);
+    	httpClient = clientBuilder.build();
+    }
+    
+	@Test
+	public void testSearch() throws Exception {
+		
+		HttpGet getRequest = new HttpGet(CONSENT_SEARCH_URI);
+		getRequest.addHeader("accept", "application/json");
+		getRequest.addHeader(ConsentManagementRestController.DEMODATA_HEADER_NAME, "true");
+		HttpResponse response = httpClient.execute(getRequest);
+		assertEquals(200, response.getStatusLine().getStatusCode());
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
+		
+		StringBuffer content = new StringBuffer();
+		String output = null;
+		
+		while ((output = br.readLine()) != null) {
+			content.append(output);
+		}
+		
+		log.info(content.toString());
+		
+	}
+
+}
