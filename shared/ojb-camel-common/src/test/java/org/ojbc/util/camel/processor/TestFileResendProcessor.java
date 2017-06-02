c7e0473776cd1cc492ba8c6e385b02d6d94bc4fa
Yogesh Chawla <yogesh@search.org>
2017-06-02 10:05:36 -0500

    Updated test to make last modified date more explicit.

--- shared/ojb-camel-common/src/test/java/org/ojbc/util/camel/processor/TestFileResendProcessor.java
+++ shared/ojb-camel-common/src/test/java/org/ojbc/util/camel/processor/TestFileResendProcessor.java
@@ -20,12 +20,19 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.io.File;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
 
 public class TestFileResendProcessor {
 
+	private static final Log log = LogFactory.getLog(TestFileResendProcessor.class);
+	
 	@Test
 	public void testFindFailedFiles()
 	{
@@ -45,6 +52,33 @@ public class TestFileResendProcessor {
 	@Test
 	public void testFileToReturn() throws Exception
 	{
+		//Explicity set the last modified date on the file to ensure the test passes
+    	try{
+
+    		File file = new File("src/test/resources/xmlInstances/booking/booking_9999999_20170323_110011192.xml");
+
+    		//print the original last modified date
+    		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+    		System.out.println("Original Last Modified Date : "
+    				+ sdf.format(file.lastModified()));
+
+    		//set this date
+    		String newLastModified = "01/31/1998";
+
+    		//need convert the above date to milliseconds in long value
+    		Date newDate = sdf.parse(newLastModified);
+    		file.setLastModified(newDate.getTime());
+
+    		//print the latest last modified date
+    		log.info("Lastest Last Modified Date : "
+    				+ sdf.format(file.lastModified()));
+
+    	}catch(ParseException e){
+
+    		e.printStackTrace();
+
+    	}
+		
 		List<File> files = FileResendProcessor.findFailedFile("9999999", "src/test/resources/xmlInstances/booking", "booking");
 		
 		File fileToResend = FileResendProcessor.returnFileToSend(files);
