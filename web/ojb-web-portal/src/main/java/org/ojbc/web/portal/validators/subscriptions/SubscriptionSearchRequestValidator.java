722e05cb50dc2381bf9660a48bb27e9461b16e3b
Haiqi Wei <haiqi@search.org>
2018-01-29 12:27:51 -0600

    Hook the advanced search form with the subscription search request validator, the controller and the the processor.

---
+++ web/ojb-web-portal/src/main/java/org/ojbc/web/portal/validators/subscriptions/SubscriptionSearchRequestValidator.java
@@ -0,0 +1,49 @@
+/*
+ * Unless explicitly acquired and licensed from Licensor under another license, the contents of
+ * this file are subject to the Reciprocal Public License ("RPL") Version 1.5, or subsequent
+ * versions as allowed by the RPL, and You may not copy or use this file in either source code
+ * or executable form, except in compliance with the terms and conditions of the RPL
+ *
+ * All software distributed under the RPL is provided strictly on an "AS IS" basis, WITHOUT
+ * WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND LICENSOR HEREBY DISCLAIMS ALL SUCH
+ * WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific language
+ * governing rights and limitations under the RPL.
+ *
+ * http://opensource.org/licenses/RPL-1.5
+ *
+ * Copyright 2012-2017 Open Justice Broker Consortium
+ */
+package org.ojbc.web.portal.validators.subscriptions;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.ojbc.web.model.subscription.search.SubscriptionSearchRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Service
+public class SubscriptionSearchRequestValidator implements Validator{
+	
+	private final Log log = LogFactory.getLog(this.getClass());	
+	
+    @Value("${sidRegexForAddSubscription:([a-zA-Z]\\d+)?}")
+    String sidRegexForSubscription;
+    
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return SubscriptionSearchRequest.class.equals(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		SubscriptionSearchRequest subscriptionSearchRequest = (SubscriptionSearchRequest) target; 
+		log.info("Validating subscription " + subscriptionSearchRequest);
+
+	}
+
+}
+
+
