77d55e4824c6ca3c7e3c8e25e6e479e24e148332
Yogesh Chawla <yogesh@search.org>
2018-04-24 22:03:49 -0500

    added additional namespaces.

--- shared/ojb-common/src/main/java/org/ojbc/util/xml/OjbcNamespaceContext.java
+++ shared/ojb-common/src/main/java/org/ojbc/util/xml/OjbcNamespaceContext.java
@@ -831,7 +831,10 @@ public final class OjbcNamespaceContext implements NamespaceContext {
 	public static final String NS_PREFIX_TRAFFIC_STOP_CODES ="ojb-ts-codes";	
 	
 	public static final String NS_PROSECUTION_DECISION_REPORT_DOC ="http://ojbc.org/IEPD/Exchange/ProsecutionDecisionReport/1.0";
-	public static final String NS_PREFIX_PROSECUTION_DECISION_REPORT_DOC ="pd-doc";	
+	public static final String NS_PREFIX_PROSECUTION_DECISION_REPORT_DOC ="pd-doc";
+
+	public static final String NS_PROSECUTION_DECISION_HAWAII_CODES_DOC ="http://ojbc.org/IEPD/Extensions/ProsecutionDecisionReportCodes/Hawaii/1.0";
+	public static final String NS_PREFIX_PROSECUTION_DECISION_HAWAII_CODES_DOC ="pd-hi-codes";
 
 	private Map<String, String> prefixToUriMap;
 	private Map<String, String> uriToPrefixMap;
@@ -842,6 +845,9 @@ public final class OjbcNamespaceContext implements NamespaceContext {
 		prefixToUriMap = new HashMap<String, String>();
 		uriToPrefixMap = new HashMap<String, String>();	
 		
+		prefixToUriMap.put(NS_PREFIX_PROSECUTION_DECISION_HAWAII_CODES_DOC, NS_PROSECUTION_DECISION_HAWAII_CODES_DOC);
+		uriToPrefixMap.put(NS_PROSECUTION_DECISION_HAWAII_CODES_DOC, NS_PREFIX_PROSECUTION_DECISION_HAWAII_CODES_DOC);
+
 		prefixToUriMap.put(NS_PREFIX_PROSECUTION_DECISION_REPORT_DOC, NS_PROSECUTION_DECISION_REPORT_DOC);
 		uriToPrefixMap.put(NS_PROSECUTION_DECISION_REPORT_DOC, NS_PREFIX_PROSECUTION_DECISION_REPORT_DOC);
 		
