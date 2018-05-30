b924ee1aa12c834e673588f717334face5fe673a
Yogesh Chawla <yogesh@search.org>
2018-05-30 15:55:25 -0500

    Added new test file and instance doc to supplement last check-in

---
+++ intermediaries/subscription-notification-service-intermediary/src/test/java/org/ojbc/intermediaries/sn/tests/FbiSubscriptionProcessorTest.java
@@ -0,0 +1,61 @@
+/*
+ * Unless explicitly acquired and licensed from Licensor under another license, the contents of
+ * this file are subject to the Reciprocal Public License ("RPL") Version 1.5, or subsequent
+ * versions as allowed by the RPL, and You may not copy or use this file in either source code
+ * or executable form, except in compliance with the terms and conditions of the RPL
+ *
+ * All software distributed under the RPL is provided strictly on an "AS IS" basis, WITHOUT
+ * WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND LICENSOR HEREBY DISCLAIMS ALL SUCH
+ * WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific language
+ * governing rights and limitations under the RPL.
+ *
+ * http://opensource.org/licenses/RPL-1.5
+ *
+ * Copyright 2012-2017 Open Justice Broker Consortium
+ */
+package org.ojbc.intermediaries.sn.tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+
+import javax.annotation.Resource;
+
+import junit.framework.Assert;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Test;
+import org.ojbc.intermediaries.sn.FbiSubscriptionProcessor;
+import org.ojbc.util.xml.XmlUtils;
+import org.w3c.dom.Document;
+
+public class FbiSubscriptionProcessorTest extends AbstractSubscriptionNotificationIntegrationTest {
+    
+    @SuppressWarnings("unused")
+	private static final Log log = LogFactory.getLog(FbiSubscriptionIntegrationTest.class);
+    
+    @Resource
+    FbiSubscriptionProcessor fbiSubscriptionProcessor;
+    
+    @Test
+    public void testRouteToProcessFbiSubscriptionRoute() throws Exception
+    {
+    	
+    	Document civilSubscriptionDocument = XmlUtils.parseFileToDocument(new File("src/test/resources/xmlInstances/fbi/OJBC_Civil_Subscription_Request_Document.xml"));
+    	
+    	Assert.assertTrue(fbiSubscriptionProcessor.routeToProcessFbiSubscriptionRoute(civilSubscriptionDocument));
+    	
+    	String base64File = XmlUtils.xPathStringSearch(civilSubscriptionDocument, "//submsg-exch:SubscriptionMessage/submsg-ext:FingerprintDocument/nc:DocumentBinary/submsg-ext:Base64BinaryObject");
+    	
+    	assertNotNull(base64File);
+    	
+    	assertEquals("StateCivilFingerPrints",  new String(Base64.decodeBase64(base64File), StandardCharsets.UTF_8));
+    	
+    }
+    
+}
