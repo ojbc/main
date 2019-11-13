6f90c248dbfe047a12f028a5ce0db462ff666f63
Yogesh Chawla <yogesh@search.org>
2019-11-13 13:32:57 -0600

    Added new service skeleton. Will fill in with additional details when adapter endpoints are defined.

---
+++ intermediaries/notify-docketing-complete-reporting-service-intermediary/src/test/java/org/ojbc/bundles/intermediaries/notify_docketing_complete/CamelContextTest.java
@@ -0,0 +1,78 @@
+/*
+ * Unless explicitly acquired and licensed from Licensor under another license, the contents of
+ * this file are subject to the Reciprocal Public License ("RPL") Version 1.5, or subsequent
+ * versions as allowed by the RPL, and You may not copy or use this file in either source code
+ * or executable form, except in compliance with the terms and conditions of the RPL
+ *
+ * All software distributed under the RPL is provided strictly on an "AS IS" basis, WITHOUT
+ * WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND LICENSOR HEREBY DISCLAIMS ALL SUCH
+ * WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific language
+ * governing rights and limitations under the RPL.
+ *
+ * http://opensource.org/licenses/RPL-1.5
+ *
+ * Copyright 2012-2017 Open Justice Broker Consortium
+ */
+package org.ojbc.bundles.intermediaries.notify_docketing_complete;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.xml.namespace.QName;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.apache.camel.EndpointInject;
+import org.apache.camel.Exchange;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.AdviceWithRouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.impl.DefaultExchange;
+import org.apache.camel.model.ModelCamelContext;
+import org.apache.camel.test.spring.CamelSpringJUnit4ClassRunner;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.cxf.binding.soap.SoapHeader;
+import org.apache.cxf.headers.Header;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ojbc.util.xml.XmlUtils;
+import org.springframework.test.context.ContextConfiguration;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+@RunWith(CamelSpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={
+		"classpath:META-INF/spring/camel-context.xml", 
+		"classpath:META-INF/spring/cxf-endpoints.xml",
+		"classpath:META-INF/spring/properties-context.xml"}) 
+public class CamelContextTest {
+
+	@SuppressWarnings("unused")
+	private static final Log log = LogFactory.getLog( CamelContextTest.class );
+	
+    @Resource
+    private ModelCamelContext context;
+	
+	@Before
+	public void setUp() throws Exception {
+			
+	}
+
+	@Test
+	public void testContextRoutes() throws Exception
+	{
+		context.start();	
+	}
+	
+	
+}
