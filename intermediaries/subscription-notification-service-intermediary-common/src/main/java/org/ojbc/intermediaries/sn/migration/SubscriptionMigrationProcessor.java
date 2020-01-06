7f0c4568bd12ad9e52ce3824c8c00e260d494ede
Yogesh Chawla <yogesh@search.org>
2020-01-06 12:50:00 -0600

    Updated with a processor which migrate subscriptions from the legacy format to the rapback database format.

---
+++ intermediaries/subscription-notification-service-intermediary-common/src/main/java/org/ojbc/intermediaries/sn/migration/SubscriptionMigrationProcessor.java
@@ -0,0 +1,248 @@
+/*
+ * Unless explicitly acquired and licensed from Licensor under another license, the contents of
+ * this file are subject to the Reciprocal Public License ("RPL") Version 1.5, or subsequent
+ * versions as allowed by the RPL, and You may not copy or use this file in either source code
+ * or executable form, except in compliance with the terms and conditions of the RPL
+ *
+ * All software distributed under the RPL is provided strictly on an "AS IS" basis, WITHOUT
+ * WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND LICENSOR HEREBY DISCLAIMS ALL SUCH
+ * WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific language
+ * governing rights and limitations under the RPL.
+ *
+ * http://opensource.org/licenses/RPL-1.5
+ *
+ * Copyright 2012-2017 Open Justice Broker Consortium
+ */
+package org.ojbc.intermediaries.sn.migration;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.camel.Exchange;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.ojbc.util.xml.subscription.Subscription;
+import org.ojbc.util.xml.subscription.SubscriptionNotificationDocumentBuilderUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.w3c.dom.Document;
+
+public class SubscriptionMigrationProcessor {
+
+	private static final Log log = LogFactory.getLog(SubscriptionMigrationProcessor.class);
+	
+	private Map<Integer, Subscription> subscriptionMap;
+	
+	private String defaultSubscriptionMigrationOri;
+	
+	public Document enrichMessageWithHeaders(Exchange ex, Subscription subscription) throws Exception
+	{
+		ex.getIn().setHeader("subscriptionOwner", subscription.getOwnerFederationId());
+		ex.getIn().setHeader("subscriptionOwnerEmailAddress", subscription.getOwnerEmailAddress());
+		ex.getIn().setHeader("subscriptionOwnerFirstName", subscription.getOwnerFirstName());
+		ex.getIn().setHeader("subscriptionOwnerLastName", subscription.getOwnerLastName());
+		
+		//We won't have ORI in all cases
+		
+		if (StringUtils.isNotBlank(subscription.getOri()))
+		{
+			ex.getIn().setHeader("subscriptionOwnerOri", subscription.getOri());	
+		}	
+		else
+		{
+			ex.getIn().setHeader("subscriptionOwnerOri", defaultSubscriptionMigrationOri);
+		}
+		
+		Document ret = SubscriptionNotificationDocumentBuilderUtils.createSubscriptionRequest(subscription, null);
+		
+		return ret;
+
+	}
+	
+	/**
+	 * A query like this is used to produce the CSV file:
+	 * 
+	 * SELECT sub.*, nm.*, ssi.* from subscription sub, notification_mechanism nm, subscription_subject_identifier ssi 
+	 *	where sub.id = nm.subscriptionId
+	 *	and  sub.id =ssi.subscriptionId
+	 *	and active = 1 and subscribingSystemIdentifier = '{http://ojbc.org/OJB_Portal/Subscriptions/1.0}OJB'
+	 * 
+	 * 
+	 * @param csvExtractLine
+	 * @throws Exception
+	 */
+	
+	public void processSubscriptionCSVRecord (String csvExtractLine) throws Exception
+	{
+		//If we see this column header, this is a column header row not data row											
+		if (StringUtils.contains(csvExtractLine, "subscribingSystemIdentifier"))
+		{
+			return;
+		}	
+		
+		if (subscriptionMap == null)
+		{
+			subscriptionMap = new HashMap<Integer, Subscription>();
+		}	
+		
+		log.debug("CSV extract entry line as string: " + csvExtractLine);
+		
+		if (StringUtils.isBlank(csvExtractLine))
+		{
+			throw new IllegalStateException("Extract entry is blank.");
+		}	
+
+        // use comma as separator
+        String[] values = csvExtractLine.split(",");
+        Integer subscriptionId = Integer.valueOf(values[0]);
+        
+        Subscription subscription = null;
+        
+        if (!subscriptionMap.containsKey(subscriptionId))
+        {
+        	subscription = new Subscription();
+    		
+            subscription.setActive(true);
+            
+            String subscriptionOwner = values[6];
+            
+//    		<!-- These headers are used when saving the subscription to the database -->
+//    		<camel:setHeader headerName="subscriptionOwner"><simple>${in.headers.saml_FederationID}</simple></camel:setHeader>
+            subscription.setOwnerFederationId(subscriptionOwner);
+            
+//    		<camel:setHeader headerName="subscriptionOwnerEmailAddress"><simple>${in.headers.saml_EmailAddress}</simple></camel:setHeader>
+
+            String subscriptionOwnerEmailAddress = StringUtils.substringAfter(subscriptionOwner, "MAINE:IDP:SOM:USER:");
+            subscription.setOwnerEmailAddress(subscriptionOwnerEmailAddress);
+            
+        	String[] subscriptionOwnerNameValues = StringUtils.substringBefore(subscriptionOwnerEmailAddress, "@").split("\\.");
+            
+        	if (subscriptionOwnerNameValues != null)
+        	{	
+        		subscription.setOwnerFirstName(subscriptionOwnerNameValues[0]);
+        		
+        		if (subscriptionOwnerNameValues.length == 2)
+        		{
+        			subscription.setOwnerLastName(subscriptionOwnerNameValues[1]);
+        		}	
+        		else if (subscriptionOwnerNameValues.length == 3)
+        		{
+        			subscription.setOwnerLastName(subscriptionOwnerNameValues[2]);
+        		}	
+        	}
+        	
+//    		<camel:setHeader headerName="subscriptionOwnerOri"><simple>${in.headers.saml_EmployerOri}</simple></camel:setHeader>
+//    		We don't have ORI so we don't set it
+        	
+        	String startDateString = values[2];
+        	
+        	if (StringUtils.isNotBlank(startDateString))
+        	{
+	        	Date startDate=new SimpleDateFormat("yyyy-MM-dd").parse(startDateString);  
+	        	subscription.setSubscriptionStartDate(startDate);
+        	}
+
+        	String endDateString = values[3];
+        	
+        	if (StringUtils.isNotBlank(endDateString) && !endDateString.equals("NULL"))
+        	{
+        		Date endDate=new SimpleDateFormat("yyyy-MM-dd").parse(endDateString);  
+	        	subscription.setSubscriptionEndDate(endDate);
+        	}
+        	
+        	subscription.setTopic(values[1]);
+        	
+        	String fullName = values[7];
+        	
+        	if (StringUtils.isNotBlank(fullName))
+        	{
+        		fullName = fullName.replaceAll("^\"|\"$", "");
+        		subscription.setFullName(fullName);	
+        		
+        	}	
+
+        	List<String> emailAddressesToNotify = new ArrayList<String>();
+        	emailAddressesToNotify.add(values[15]);
+        	subscription.setEmailList(emailAddressesToNotify);
+        	
+        	setSubjectIdentifierValues(values, subscription);
+        	
+            subscriptionMap.put(subscriptionId, subscription);
+
+            
+        }	
+        else
+        {
+        	subscription = subscriptionMap.get(subscriptionId);
+        	
+        	setSubjectIdentifierValues(values, subscription);
+        }	
+        
+	}
+
+	private void setSubjectIdentifierValues(String[] values, Subscription subscription) throws ParseException {
+		if (!subscription.getEmailList().contains(values[15]))
+		{
+			subscription.getEmailList().add(values[15]);
+		}
+
+		if (values[18].equals("firstName"))
+		{	
+			subscription.setFirstName(values[19]);
+		}
+
+		if (values[18].equals("lastName"))
+		{	
+			subscription.setLastName(values[19]);
+		}
+
+		if (values[18].equals("SID"))
+		{	
+			subscription.setStateId(values[19]);
+		}
+
+		if (values[18].equals("dateOfBirth"))
+		{	
+			Date dateOfBirth=new SimpleDateFormat("yyyy-MM-dd").parse(values[19]);
+			subscription.setDateOfBirth(dateOfBirth);
+		}
+	}
+	
+	public List<Subscription> getCompleteSubscriptions()
+	{
+		List<Subscription> subscriptions = new ArrayList<Subscription>(subscriptionMap.values());
+		return subscriptions;
+	}
+	
+	public void clearSubscriptionMap()
+	{
+		if (subscriptionMap != null)
+		{	
+			subscriptionMap.clear();
+		}
+	}
+
+	public Map<Integer, Subscription> getSubscriptionMap() {
+		return subscriptionMap;
+	}
+
+	public void setSubscriptionMap(Map<Integer, Subscription> subscriptionMap) {
+		this.subscriptionMap = subscriptionMap;
+	}
+
+	public String getDefaultSubscriptionMigrationOri() {
+		return defaultSubscriptionMigrationOri;
+	}
+
+	public void setDefaultSubscriptionMigrationOri(String defaultSubscriptionMigrationOri) {
+		this.defaultSubscriptionMigrationOri = defaultSubscriptionMigrationOri;
+	}
+
+	
+}
---
+++ intermediaries/subscription-notification-service-intermediary-common/src/test/java/org/ojbc/intermediaries/sn/dao/migration/TestSubscriptionMigrationProcessor.java
@@ -0,0 +1,148 @@
+/*
+ * Unless explicitly acquired and licensed from Licensor under another license, the contents of
+ * this file are subject to the Reciprocal Public License ("RPL") Version 1.5, or subsequent
+ * versions as allowed by the RPL, and You may not copy or use this file in either source code
+ * or executable form, except in compliance with the terms and conditions of the RPL
+ *
+ * All software distributed under the RPL is provided strictly on an "AS IS" basis, WITHOUT
+ * WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND LICENSOR HEREBY DISCLAIMS ALL SUCH
+ * WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific language
+ * governing rights and limitations under the RPL.
+ *
+ * http://opensource.org/licenses/RPL-1.5
+ *
+ * Copyright 2012-2017 Open Justice Broker Consortium
+ */
+package org.ojbc.intermediaries.sn.dao.migration;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.DefaultExchange;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ojbc.intermediaries.sn.migration.SubscriptionMigrationProcessor;
+import org.ojbc.util.xml.XmlUtils;
+import org.ojbc.util.xml.subscription.Subscription;
+import org.ojbc.util.xml.subscription.SubscriptionNotificationDocumentBuilderUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={
+		"classpath:META-INF/spring/test-application-context.xml",
+		"classpath:META-INF/spring/h2-mock-database-application-context.xml",
+		"classpath:META-INF/spring/h2-mock-database-context-subscription.xml",
+		})
+@DirtiesContext
+public class TestSubscriptionMigrationProcessor {
+    
+    @SuppressWarnings("unused")
+	private static final Log log = LogFactory.getLog(TestSubscriptionMigrationProcessor.class);
+	
+	@Autowired
+    private SubscriptionMigrationProcessor subscriptionMigrationProcessor;
+	
+    @Test
+    public void testSubscriptionMigration() throws Exception {
+    	
+    	subscriptionMigrationProcessor.clearSubscriptionMap();
+    	
+    	String csvLine1 = "84172,{http://ojbc.org/wsn/topics}:person/criminalHistoryCycleTrackingIdentifierAssignment,2017-11-20,NULL,2017-11-20,{http://ojbc.org/OJB_Portal/Subscriptions/1.0}OJB,MAINE:IDP:SOM:USER:First.Person@local.gov,First Subject,1,NULL,NULL,2017-11-20 15:36,109090,84172,email,First.Person@local.gov,338034,84172,dateOfBirth,1960-01-02"; 
+    	subscriptionMigrationProcessor.processSubscriptionCSVRecord(csvLine1);
+    	
+    	String csvLine2 = "84172,{http://ojbc.org/wsn/topics}:person/criminalHistoryCycleTrackingIdentifierAssignment,2017-11-20,NULL,2017-11-20,{http://ojbc.org/OJB_Portal/Subscriptions/1.0}OJB,MAINE:IDP:SOM:USER:First.Person@local.gov,First Subject,1,NULL,NULL,2017-11-20 15:36,109090,84172,email,First.Person@local.gov,338031,84172,firstName,First"; 
+    	subscriptionMigrationProcessor.processSubscriptionCSVRecord(csvLine2);
+
+    	String csvLine3 = "84172,{http://ojbc.org/wsn/topics}:person/criminalHistoryCycleTrackingIdentifierAssignment,2017-11-20,NULL,2017-11-20,{http://ojbc.org/OJB_Portal/Subscriptions/1.0}OJB,MAINE:IDP:SOM:USER:First.Person@local.gov,First Subject,1,NULL,NULL,2017-11-20 15:36,109090,84172,email,First.Person@local.gov,338032,84172,lastName,Subject"; 
+    	subscriptionMigrationProcessor.processSubscriptionCSVRecord(csvLine3);
+
+    	String csvLine4 = "84172,{http://ojbc.org/wsn/topics}:person/criminalHistoryCycleTrackingIdentifierAssignment,2017-11-20,NULL,2017-11-20,{http://ojbc.org/OJB_Portal/Subscriptions/1.0}OJB,MAINE:IDP:SOM:USER:First.Person@local.gov,First Subject,1,NULL,NULL,2017-11-20 15:36,109090,84172,email,First.Person@local.gov,338033,84172,subscriptionQualifier,977b0ff6-246e-413d-abc8-ee7cc1ee23ee"; 
+    	subscriptionMigrationProcessor.processSubscriptionCSVRecord(csvLine4);
+
+    	String csvLine5 = "100843,{http://ojbc.org/wsn/topics}:person/criminalHistoryCycleTrackingIdentifierAssignment,2018-03-02,2019-03-02,2018-03-02,{http://ojbc.org/OJB_Portal/Subscriptions/1.0}OJB,MAINE:IDP:SOM:USER:Second.2.person@local.gov,Second Subject,1,NULL,NULL,2018-03-02 13:59,130712,100843,email,third.person@local.gov,406264,100843,dateOfBirth,1990-04-01"; 
+    	subscriptionMigrationProcessor.processSubscriptionCSVRecord(csvLine5);
+
+    	String csvLine6 = "100843,{http://ojbc.org/wsn/topics}:person/criminalHistoryCycleTrackingIdentifierAssignment,2018-03-02,2019-03-02,2018-03-02,{http://ojbc.org/OJB_Portal/Subscriptions/1.0}OJB,MAINE:IDP:SOM:USER:Second.2.person@local.gov,Second Subject,1,NULL,NULL,2018-03-02 13:59,130712,100843,email,third.person@local.gov,406261,100843,firstName,Second"; 
+    	subscriptionMigrationProcessor.processSubscriptionCSVRecord(csvLine6);
+
+    	String csvLine7 = "100843,{http://ojbc.org/wsn/topics}:person/criminalHistoryCycleTrackingIdentifierAssignment,2018-03-02,2019-03-02,2018-03-02,{http://ojbc.org/OJB_Portal/Subscriptions/1.0}OJB,MAINE:IDP:SOM:USER:Second.2.person@local.gov,Second Subject,1,NULL,NULL,2018-03-02 13:59,130712,100843,email,third.person@local.gov,406262,100843,lastName,Subject"; 
+    	subscriptionMigrationProcessor.processSubscriptionCSVRecord(csvLine7);
+
+    	String csvLine8 = "100843,{http://ojbc.org/wsn/topics}:person/criminalHistoryCycleTrackingIdentifierAssignment,2018-03-02,2019-03-02,2018-03-02,{http://ojbc.org/OJB_Portal/Subscriptions/1.0}OJB,MAINE:IDP:SOM:USER:Second.2.person@local.gov,Second Subject,1,NULL,NULL,2018-03-02 13:59,130712,100843,email,third.person@local.gov,406263,100843,subscriptionQualifier,e31bb757-5ac8-4f13-adce-2879bd76b97d"; 
+    	subscriptionMigrationProcessor.processSubscriptionCSVRecord(csvLine8);
+
+    	String csvLine9 = "100843,{http://ojbc.org/wsn/topics}:person/criminalHistoryCycleTrackingIdentifierAssignment,2018-03-02,2019-03-02,2018-03-02,{http://ojbc.org/OJB_Portal/Subscriptions/1.0}OJB,MAINE:IDP:SOM:USER:Second.2.person@local.gov,Second Subject,1,NULL,NULL,2018-03-02 13:59,130711,100843,email,Second.2.person@local.gov,406264,100843,dateOfBirth,1990-04-01"; 
+    	subscriptionMigrationProcessor.processSubscriptionCSVRecord(csvLine9);
+
+    	String csvLine10 = "100843,{http://ojbc.org/wsn/topics}:person/criminalHistoryCycleTrackingIdentifierAssignment,2018-03-02,2019-03-02,2018-03-02,{http://ojbc.org/OJB_Portal/Subscriptions/1.0}OJB,MAINE:IDP:SOM:USER:Second.2.person@local.gov,Second Subject,1,NULL,NULL,2018-03-02 13:59,130711,100843,email,Second.2.person@local.gov,406261,100843,firstName,Second"; 
+    	subscriptionMigrationProcessor.processSubscriptionCSVRecord(csvLine10);
+
+    	String csvLine11 = "100843,{http://ojbc.org/wsn/topics}:person/criminalHistoryCycleTrackingIdentifierAssignment,2018-03-02,2019-03-02,2018-03-02,{http://ojbc.org/OJB_Portal/Subscriptions/1.0}OJB,MAINE:IDP:SOM:USER:Second.2.person@local.gov,Second Subject,1,NULL,NULL,2018-03-02 13:59,130711,100843,email,Second.2.person@local.gov,406262,100843,lastName,Subject"; 
+    	subscriptionMigrationProcessor.processSubscriptionCSVRecord(csvLine11);
+
+    	String csvLine12 = "100843,{http://ojbc.org/wsn/topics}:person/criminalHistoryCycleTrackingIdentifierAssignment,2018-03-02,2019-03-02,2018-03-02,{http://ojbc.org/OJB_Portal/Subscriptions/1.0}OJB,MAINE:IDP:SOM:USER:Second.2.person@local.gov,Second Subject,1,NULL,NULL,2018-03-02 13:59,130711,100843,email,Second.2.person@local.gov,406263,100843,subscriptionQualifier,e31bb757-5ac8-4f13-adce-2879bd76b97d"; 
+    	subscriptionMigrationProcessor.processSubscriptionCSVRecord(csvLine12);
+
+    	log.info("Subscriptions: " + subscriptionMigrationProcessor.getSubscriptionMap());
+    	
+    	assertEquals(2, subscriptionMigrationProcessor.getSubscriptionMap().size());
+    	
+    	Map<Integer, Subscription> subscriptionsMap = subscriptionMigrationProcessor.getSubscriptionMap();
+    	
+    	Subscription subscription1 = subscriptionsMap.get(84172);
+    	assertNotNull(subscription1);
+    	assertEquals(1, subscription1.getEmailList().size());
+    	
+    	
+    	Subscription subscription2 = subscriptionsMap.get(100843);
+    	assertNotNull(subscription2);
+    	assertEquals(2, subscription2.getEmailList().size());
+
+        // using for-each loop for iteration over Map.entrySet() 
+        for (Map.Entry<Integer,Subscription> entry : subscriptionsMap.entrySet())  
+        {	
+        	Subscription subToPrint = entry.getValue();
+            XmlUtils.printNode(SubscriptionNotificationDocumentBuilderUtils.createSubscriptionRequest(subToPrint, null)); 
+            
+            CamelContext ctx = new DefaultCamelContext(); 
+            Exchange ex = new DefaultExchange(ctx);
+            
+            subscriptionMigrationProcessor.enrichMessageWithHeaders(ex, subToPrint);
+            
+            if (entry.getKey().equals(84172))
+            {
+            	assertEquals("MAINE:IDP:SOM:USER:First.Person@local.gov", ex.getIn().getHeader("subscriptionOwner"));
+            	assertEquals("First.Person@local.gov", ex.getIn().getHeader("subscriptionOwnerEmailAddress"));
+            	assertEquals("First", ex.getIn().getHeader("subscriptionOwnerFirstName"));
+            	assertEquals("Person", ex.getIn().getHeader("subscriptionOwnerLastName"));
+            	assertEquals("defaultORI", ex.getIn().getHeader("subscriptionOwnerOri"));
+            }
+            
+            if (entry.getKey().equals(100843))
+            {
+            	assertEquals("MAINE:IDP:SOM:USER:Second.2.person@local.gov", ex.getIn().getHeader("subscriptionOwner"));
+            	assertEquals("Second.2.person@local.gov", ex.getIn().getHeader("subscriptionOwnerEmailAddress"));
+            	assertEquals("Second", ex.getIn().getHeader("subscriptionOwnerFirstName"));
+            	assertEquals("person", ex.getIn().getHeader("subscriptionOwnerLastName"));
+            	assertEquals("defaultORI", ex.getIn().getHeader("subscriptionOwnerOri"));
+            	
+            }	
+        }
+        
+        List<Subscription> subscriptions = subscriptionMigrationProcessor.getCompleteSubscriptions();
+        
+	}
+
+}
