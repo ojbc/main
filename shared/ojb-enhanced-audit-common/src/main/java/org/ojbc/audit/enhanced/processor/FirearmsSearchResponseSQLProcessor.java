7c00f97441b8d20c2eca2da04e9d6a873546fb5f
Yogesh Chawla <yogesh@search.org>
2020-06-15 14:09:12 -0500

    Adding firearms search results.

--- shared/ojb-enhanced-audit-common/src/main/java/org/ojbc/audit/enhanced/processor/FirearmsSearchResponseSQLProcessor.java
+++ shared/ojb-enhanced-audit-common/src/main/java/org/ojbc/audit/enhanced/processor/FirearmsSearchResponseSQLProcessor.java
@@ -23,7 +23,6 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.ojbc.audit.enhanced.dao.EnhancedAuditDAO;
 import org.ojbc.audit.enhanced.dao.model.FirearmSearchResult;
-import org.ojbc.audit.enhanced.dao.model.PersonSearchResult;
 import org.w3c.dom.Document;
 
 public class FirearmsSearchResponseSQLProcessor extends AbstractFirearmSearchResponseProcessor{
