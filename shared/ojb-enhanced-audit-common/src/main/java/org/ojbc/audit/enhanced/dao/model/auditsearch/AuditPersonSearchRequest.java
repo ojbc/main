db3a39ae6876533493553d4206378f1bb89375ba
Yogesh Chawla <yogesh@search.org>
2020-06-22 13:34:11 -0500

    Added back end method to search for person searches by person name parameters.

---
+++ shared/ojb-enhanced-audit-common/src/main/java/org/ojbc/audit/enhanced/dao/model/auditsearch/AuditPersonSearchRequest.java
@@ -0,0 +1,44 @@
+/*
+ * Unless explicitly acquired and licensed from Licensor under another license, the contents of
+ * this file are subject to the Reciprocal Public License ("RPL") Version 1.5, or subsequent
+ * versions as allowed by the RPL, and You may not copy or use this file in either source code
+ * or executable form, except in compliance with the terms and conditions of the RPL
+ *
+ * All software distributed under the RPL is provided strictly on an "AS IS" basis, WITHOUT
+ * WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND LICENSOR HEREBY DISCLAIMS ALL SUCH
+ * WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific language
+ * governing rights and limitations under the RPL.
+ *
+ * http://opensource.org/licenses/RPL-1.5
+ *
+ * Copyright 2012-2017 Open Justice Broker Consortium
+ */
+package org.ojbc.audit.enhanced.dao.model.auditsearch;
+
+public class AuditPersonSearchRequest extends AuditSearchRequest {
+
+	private String personFirstName;
+	private String personMiddleName;
+	private String personLastName;
+	
+	public String getPersonFirstName() {
+		return personFirstName;
+	}
+	public void setPersonFirstName(String personFirstName) {
+		this.personFirstName = personFirstName;
+	}
+	public String getPersonMiddleName() {
+		return personMiddleName;
+	}
+	public void setPersonMiddleName(String personMiddleName) {
+		this.personMiddleName = personMiddleName;
+	}
+	public String getPersonLastName() {
+		return personLastName;
+	}
+	public void setPersonLastName(String personLastName) {
+		this.personLastName = personLastName;
+	}
+	
+}
