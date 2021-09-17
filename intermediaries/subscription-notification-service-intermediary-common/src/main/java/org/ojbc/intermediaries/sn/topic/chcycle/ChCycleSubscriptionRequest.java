0cb701494bb74443ba0476bb1cbf4ff7c273dcb8
michaelmackay <michaelmackay@mmackay.lan>
2021-09-17 12:29:03 -0400

    Added new topic and route for Warrant Files.

--- intermediaries/subscription-notification-service-intermediary-common/src/main/java/org/ojbc/intermediaries/sn/topic/chcycle/ChCycleSubscriptionRequest.java
+++ intermediaries/subscription-notification-service-intermediary-common/src/main/java/org/ojbc/intermediaries/sn/topic/chcycle/ChCycleSubscriptionRequest.java
@@ -18,6 +18,7 @@ package org.ojbc.intermediaries.sn.topic.chcycle;
 
 import java.util.HashMap;
 
+
 import org.apache.camel.Message;
 import org.ojbc.intermediaries.sn.SubscriptionNotificationConstants;
 import org.ojbc.intermediaries.sn.subscription.SubscriptionRequest;
