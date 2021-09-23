bdc63995289576a207915e2d14dab30ac6901901
michaelmackay <michaelmackay@mmackay.lan>
2021-09-23 16:58:39 -0400

    Full email body for test message set up.

--- intermediaries/subscription-notification-service-intermediary-common/src/main/java/org/ojbc/intermediaries/sn/notification/EmailNotification.java
+++ intermediaries/subscription-notification-service-intermediary-common/src/main/java/org/ojbc/intermediaries/sn/notification/EmailNotification.java
@@ -176,6 +176,7 @@ public final class EmailNotification {
         copy.notificationRequest = notificationRequest;
         copy.subscriptionCategoryCode=subscriptionCategoryCode;
         copy.subscriptionSubjectIdentifiers = subscriptionSubjectIdentifiers;
+        copy.subscription = subscription;
         return copy;
     }
 
