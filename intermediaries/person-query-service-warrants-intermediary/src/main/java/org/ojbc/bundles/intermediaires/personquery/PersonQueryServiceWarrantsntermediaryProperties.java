de4038254dc83603917ef8266293cbf5d58e7784
Haiqi Wei <haiqi@search.org>
2021-10-11 11:52:57 -0500

    Upgrade to spring boot 2.5.2.

---
+++ intermediaries/person-query-service-warrants-intermediary/src/main/java/org/ojbc/bundles/intermediaires/personquery/PersonQueryServiceWarrantsntermediaryProperties.java
@@ -0,0 +1,18 @@
+package org.ojbc.bundles.intermediaires.personquery;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "person-query-service")
+public class PersonQueryServiceWarrantsntermediaryProperties {
+	private String dbAuditLog = "false";
+
+	public String getDbAuditLog() {
+		return dbAuditLog;
+	}
+
+	public void setDbAuditLog(String dbAuditLog) {
+		this.dbAuditLog = dbAuditLog;
+	} 
+}
--- intermediaries/person-query-service-warrants-intermediary/src/test/java/org/ojbc/bundles/intermediaries/TestPersonQueryServiceWarrants.java
+++ intermediaries/person-query-service-warrants-intermediary/src/test/java/org/ojbc/bundles/intermediaries/TestPersonQueryServiceWarrants.java
@@ -16,23 +16,21 @@
  */
 package org.ojbc.bundles.intermediaries;
 
-import static junit.framework.Assert.assertTrue;
-
-import org.apache.camel.test.spring.CamelSpringJUnit4ClassRunner;
-import org.apache.camel.test.spring.UseAdviceWith;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
+import org.apache.camel.test.spring.junit5.UseAdviceWith;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
+import org.ojbc.bundles.intermediaires.personquery.PersonQueryServiceWarrantsIntermediaryApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ActiveProfiles;
 
-@UseAdviceWith	// NOTE: this causes Camel contexts to not start up automatically
-@RunWith(CamelSpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={
-		"classpath:META-INF/spring/camel-context.xml", 
-		"classpath:META-INF/spring/cxf-endpoints.xml",
-		"classpath:META-INF/spring/extensible-beans.xml",	
-		"classpath:META-INF/spring/jetty-server.xml",
-		"classpath:META-INF/spring/local-osgi-context.xml",
-		"classpath:META-INF/spring/properties-context.xml"}) 
+@CamelSpringBootTest
+@SpringBootTest(classes=PersonQueryServiceWarrantsIntermediaryApplication.class)
+@ActiveProfiles("dev")
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+@UseAdviceWith
 public class TestPersonQueryServiceWarrants {
 
     @Test
