6210744090db53360a302fd8aba2513ff36a73c9
michaelmackay <michaelmackay@mmackay.lan>
2021-12-17 15:34:09 -0500

    Modified pom.xml for JAR config/log4j upgrade and missing license headers.

---
+++ intermediaries/policy-acknowledgement-service-intermediary/java/org/ojbc/bundles/intermediaries/TestFirearmRegistrationQueryServiceIntermediary.java
@@ -0,0 +1,45 @@
+/*
+ * Unless explicitly acquired and licensed from Licensor under another license, the contents of
+ * this file are subject to the Reciprocal Public License ("RPL") Version 1.5, or subsequent
+ * versions as allowed by the RPL, and You may not copy or use this file in either source code
+ * or executable form, except in compliance with the terms and conditions of the RPL
+ *
+ * All software distributed under the RPL is provided strictly on an "AS IS" basis, WITHOUT
+ * WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND LICENSOR HEREBY DISCLAIMS ALL SUCH
+ * WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific language
+ * governing rights and limitations under the RPL.
+ *
+ * http://opensource.org/licenses/RPL-1.5
+ *
+ * Copyright 2012-2017 Open Justice Broker Consortium
+ */
+package org.ojbc.bundles.intermediaries;
+
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
+import org.apache.camel.test.spring.junit5.UseAdviceWith;
+import org.junit.jupiter.api.Test;
+import org.ojbc.bundles.intermediaries.firearmregistration.FirearmRegistrationQueryRequestServiceIntermediaryApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ActiveProfiles;
+
+@CamelSpringBootTest
+@SpringBootTest(classes=FirearmRegistrationQueryRequestServiceIntermediaryApplication.class)
+@ActiveProfiles("dev")
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+@UseAdviceWith
+public class TestFirearmRegistrationQueryServiceIntermediary{
+
+
+    @Test
+    public void testApplicationStartup()
+    {
+    	assertTrue(true);
+    }	
+}
