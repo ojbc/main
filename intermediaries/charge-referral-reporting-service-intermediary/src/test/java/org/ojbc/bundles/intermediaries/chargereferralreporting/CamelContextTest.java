19381698ad2e01c765efe892a20fcdd7756ddd95
michaelmackay <michaelmackay@192.168.1.47>
2022-02-11 14:18:31 -0500

    Upgraded charge-referral-reporting-service-intermediary.

--- intermediaries/charge-referral-reporting-service-intermediary/src/test/java/org/ojbc/bundles/intermediaries/chargereferralreporting/CamelContextTest.java
+++ intermediaries/charge-referral-reporting-service-intermediary/src/test/java/org/ojbc/bundles/intermediaries/chargereferralreporting/CamelContextTest.java
@@ -32,31 +32,34 @@ import org.apache.camel.EndpointInject;
 import org.apache.camel.Exchange;
 import org.apache.camel.Produce;
 import org.apache.camel.ProducerTemplate;
-import org.apache.camel.builder.AdviceWithRouteBuilder;
+import org.apache.camel.builder.AdviceWith;
 import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.impl.DefaultExchange;
 import org.apache.camel.model.ModelCamelContext;
-import org.apache.camel.test.spring.CamelSpringJUnit4ClassRunner;
+import org.apache.camel.support.DefaultExchange;
+import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
+import org.apache.camel.test.spring.junit5.UseAdviceWith;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.cxf.binding.soap.SoapHeader;
 import org.apache.cxf.headers.Header;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.ojbc.bundles.intermediaries.chargereferral.ChargeReferralReportingServiceIntermediaryApplication;
 import org.ojbc.util.xml.XmlUtils;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ActiveProfiles;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
-@RunWith(CamelSpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={
-		"classpath:META-INF/spring/camel-context.xml", 
-		"classpath:META-INF/spring/cxf-endpoints.xml",
-		"classpath:META-INF/spring/jetty-server.xml",
-		"classpath:META-INF/spring/properties-context.xml"}) 
+@UseAdviceWith
+@CamelSpringBootTest
+@SpringBootTest(classes=ChargeReferralReportingServiceIntermediaryApplication.class)
+@ActiveProfiles("dev")
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD) 
 public class CamelContextTest {
 
 	@SuppressWarnings("unused")
@@ -72,36 +75,27 @@ public class CamelContextTest {
     @Produce
     protected ProducerTemplate template;
     
-    @EndpointInject(uri = "mock:cxf:bean:chargeReferralReportingAdapterService")
+    @EndpointInject(value = "mock:cxf:bean:chargeReferralReportingAdapterService")
     protected MockEndpoint chargeReferralReportingAdapterMockEndpoint;
     
-    @EndpointInject(uri = "mock:log:org.ojbc.bundles.intermediaries.chargereferralreporting")
+    @EndpointInject(value = "mock:log:org.ojbc.bundles.intermediaries.chargereferralreporting")
     protected MockEndpoint loggingEndpoint;
 
     
-	@Before
+	@BeforeEach
 	public void setUp() throws Exception {
 		
     	//We replace the 'from' web service endpoint with a direct endpoint we call in our test
 		//We mock the 'log' endpoint to test against.
-    	context.getRouteDefinition("ChargeReferralReporting_Route").adviceWith(context, new AdviceWithRouteBuilder() {
-    	    @Override
-    	    public void configure() throws Exception {
-    	    	// The line below allows us to bypass CXF and send a message directly into the route
-    	    	replaceFromWith("direct:ChargeReferralReportingEndpoint");
-    	    	mockEndpoints("log:org.ojbc.bundles.intermediaries.chargereferralreporting*");    	    	
-    	    }              
+    	AdviceWith.adviceWith(context, "ChargeReferralReporting_Route", route -> {
+    		route.replaceFromWith("direct:ChargeReferralReportingEndpoint");
+    		route.mockEndpoints("log:org.ojbc.bundles.intermediaries.chargereferralreporting*");
     	});
     	
     	// mock the web service endpoints
-    	context.getRouteDefinition("CallChargeReferralAdapter_Route").adviceWith(context, new AdviceWithRouteBuilder() {
-    	    @Override
-    	    public void configure() throws Exception {
-    	    	
-    	    	// mock the adapter endpoint
-    	    	mockEndpointsAndSkip("cxf:bean:chargeReferralReportingAdapterService*");    	    	
-    	    }              
-    	});    	
+    	AdviceWith.adviceWith(context, "CallChargeReferralAdapter_Route", route -> {
+    		route.weaveById("chargeReferralReportingAdapterEndpoint").replace().to(chargeReferralReportingAdapterMockEndpoint);
+    	});
     	
 		context.start();		
 	}
