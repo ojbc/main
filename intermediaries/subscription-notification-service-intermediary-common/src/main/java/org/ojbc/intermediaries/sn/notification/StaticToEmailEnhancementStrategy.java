8dab0435c3fb31c4f5bce566c6c709dfe490da3b
Haiqi Wei <haiqi@search.org>
2022-09-15 14:04:14 -0500

    Use commons lang3 instead.

--- intermediaries/subscription-notification-service-intermediary-common/src/main/java/org/ojbc/intermediaries/sn/notification/StaticToEmailEnhancementStrategy.java
+++ intermediaries/subscription-notification-service-intermediary-common/src/main/java/org/ojbc/intermediaries/sn/notification/StaticToEmailEnhancementStrategy.java
@@ -16,7 +16,7 @@
  */
 package org.ojbc.intermediaries.sn.notification;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
