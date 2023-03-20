ec53b10553d60fe6489684c70c02627b591c054c
Haiqi Wei <haiqi@search.org>
2023-03-20 11:29:28 -0500

    Add a StateMap emailEnhancementStrategy.
    Merge remote-tracking branch 'origin/preTotpChanges-branch' into camel3

---
+++ intermediaries/subscription-notification-service-intermediary-common/src/main/java/org/ojbc/intermediaries/sn/notification/StateMapEmailEnhancementStrategy.java
@@ -0,0 +1,57 @@
+/*
+ * Unless explicitly acquired and licensed from Licensor under another license, the contents of
+ * this file are subject to the Reciprocal Public License ("RPL") Version 1.5, or subsequent
+ * versions as allowed by the RPL, and You may not copy or use this file in either source code
+ * or executable form, except in compliance with the terms and conditions of the RPL
+ *
+ * All software distributed under the RPL is provided strictly on an "AS IS" basis, WITHOUT
+ * WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND LICENSOR HEREBY DISCLAIMS ALL SUCH
+ * WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific language
+ * governing rights and limitations under the RPL.
+ *
+ * http://opensource.org/licenses/RPL-1.5
+ *
+ * Copyright 2012-2017 Open Justice Broker Consortium
+ */
+package org.ojbc.intermediaries.sn.notification;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A composite strategy that selects the specific strategy to use based on topic.
+ *
+ */
+public class StateMapEmailEnhancementStrategy implements EmailEnhancementStrategy {
+	
+	private EmailEnhancementStrategy defaultStrategy = new DefaultEmailEnhancementStrategy();
+	
+	private Map<String, EmailEnhancementStrategy> stateStrategyMap = new HashMap<String, EmailEnhancementStrategy>();
+
+	public EmailEnhancementStrategy getDefaultStrategy() {
+		return defaultStrategy;
+	}
+
+	public void setDefaultStrategy(EmailEnhancementStrategy defaultStrategy) {
+		this.defaultStrategy = defaultStrategy;
+	}
+
+	public Map<String, EmailEnhancementStrategy> getStateStrategyMap() {
+		return stateStrategyMap;
+	}
+
+	public void setStateStrategyMap(Map<String, EmailEnhancementStrategy> stateStrategyMap) {
+		this.stateStrategyMap = stateStrategyMap;
+	}
+
+	@Override
+	public EmailNotification enhanceEmail(EmailNotification emailNotification) {
+		EmailEnhancementStrategy strategy = stateStrategyMap.get(emailNotification.getSubscription().getState());
+		if (strategy == null) {
+			strategy = defaultStrategy;
+		}
+		return strategy.enhanceEmail(emailNotification);
+	}
+
+}
