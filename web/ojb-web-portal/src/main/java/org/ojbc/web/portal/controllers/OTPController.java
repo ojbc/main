2c6562f463e9b7cf0d26a65c249506c74b415cc6
Haiqi Wei <haiqi@search.org>
2023-04-19 10:34:13 -0500

    Minor. organized imports.

--- web/ojb-web-portal/src/main/java/org/ojbc/web/portal/controllers/OTPController.java
+++ web/ojb-web-portal/src/main/java/org/ojbc/web/portal/controllers/OTPController.java
@@ -33,7 +33,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.w3c.dom.Element;
 
 @Controller
