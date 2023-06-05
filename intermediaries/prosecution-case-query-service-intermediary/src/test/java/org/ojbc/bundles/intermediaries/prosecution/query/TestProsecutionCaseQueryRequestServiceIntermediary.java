e24d015257362abcc69867538ee153a2dad9d853
Haiqi Wei <haiqi@search.org>
2023-06-05 11:09:53 -0500

    Code cleanup.

--- intermediaries/prosecution-case-query-service-intermediary/src/test/java/org/ojbc/bundles/intermediaries/prosecution/query/TestProsecutionCaseQueryRequestServiceIntermediary.java
+++ intermediaries/prosecution-case-query-service-intermediary/src/test/java/org/ojbc/bundles/intermediaries/prosecution/query/TestProsecutionCaseQueryRequestServiceIntermediary.java
@@ -14,7 +14,7 @@
  *
  * Copyright 2012-2017 Open Justice Broker Consortium
  */
-package org.ojbc.bundles.intermediaries.courtcase.search;
+package org.ojbc.bundles.intermediaries.prosecution.query;
 
 import static org.junit.Assert.assertTrue;
 
