fbc2e350036426795aefb603b8b83428a7bcb243
Haiqi Wei <haiqi@search.org>
2023-09-28 13:17:21 -0500

    Organized imports.

--- intermediaries/policy-acknowledgement-service-intermediary/src/main/java/org/ojbc/policyacknowledgement/dao/PolicyDAOImpl.java
+++ intermediaries/policy-acknowledgement-service-intermediary/src/main/java/org/ojbc/policyacknowledgement/dao/PolicyDAOImpl.java
@@ -24,8 +24,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.sql.DataSource;
-
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
