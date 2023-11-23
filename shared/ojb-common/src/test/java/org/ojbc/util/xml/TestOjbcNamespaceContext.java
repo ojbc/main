b029b87516089fff695581109db8bd23743d87c4
Haiqi Wei <haiqi@search.org>
2023-11-22 20:09:13 -0600

    Minor, organized imports.

--- shared/ojb-common/src/test/java/org/ojbc/util/xml/TestOjbcNamespaceContext.java
+++ shared/ojb-common/src/test/java/org/ojbc/util/xml/TestOjbcNamespaceContext.java
@@ -18,7 +18,6 @@ package org.ojbc.util.xml;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 import javax.xml.parsers.DocumentBuilder;
