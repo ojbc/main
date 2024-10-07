e74107b7ff263c7b33d41bf60a4520b1ac2faf9f
mmackaysearch <mmackay@search.org>
2024-10-07 13:35:01 -0400

    Updated FBI EBTS adapter to use quartz timer for subscription report.

--- adapters/fbi-electronic-biometric-transmission-specification-adapter/src/main/java/org/ojbc/bundles/adapters/fbi/ebts/processor/SubscriptionReportFileProcessor.java
+++ adapters/fbi-electronic-biometric-transmission-specification-adapter/src/main/java/org/ojbc/bundles/adapters/fbi/ebts/processor/SubscriptionReportFileProcessor.java
@@ -5,15 +5,14 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.apache.camel.Exchange;
 import org.apache.camel.Header;
 import org.apache.commons.io.FileUtils;
 
 public class SubscriptionReportFileProcessor {
 	
-	public void copyFileToInputDirectory(Exchange exchange, @Header("inputMessageFilePath") String inputDirString) throws IOException {
+	public void copyFileToInputDirectory(@Header("subscriptionReportFilePath") String subscriptionReportFilePath, @Header("inputMessageFilePath") String inputDirString) throws IOException {
 		
-		File subscriptionReport = (File) exchange.getIn().getBody();
+		File subscriptionReport = new File(subscriptionReportFilePath);
 		
 		String date = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
 		File subscriptionReportDated = new File("Generate_Report_" + date);
