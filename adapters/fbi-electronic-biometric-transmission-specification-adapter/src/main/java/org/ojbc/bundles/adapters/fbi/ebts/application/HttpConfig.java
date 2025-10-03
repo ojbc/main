54cdf5e48f313f98cc6190634b099235fd7636cc
Haiqi Wei <haiqi@search.org>
2025-10-03 08:06:24 -0500

    Add https component connection timeout properties.

--- adapters/fbi-electronic-biometric-transmission-specification-adapter/src/main/java/org/ojbc/bundles/adapters/fbi/ebts/application/HttpConfig.java
+++ adapters/fbi-electronic-biometric-transmission-specification-adapter/src/main/java/org/ojbc/bundles/adapters/fbi/ebts/application/HttpConfig.java
@@ -1,77 +1,94 @@
 package org.ojbc.bundles.adapters.fbi.ebts.application;
 
-import java.io.IOException;
-import java.security.GeneralSecurityException;
+import java.lang.reflect.Field;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.camel.CamelContext;
-import org.apache.camel.component.http.HttpClientConfigurer;
 import org.apache.camel.component.http.HttpComponent;
+import org.apache.camel.support.jsse.KeyManagersParameters;
+import org.apache.camel.support.jsse.KeyStoreParameters;
 import org.apache.camel.support.jsse.SSLContextParameters;
-import org.apache.http.client.config.RequestConfig;
+import org.apache.camel.support.jsse.TrustManagersParameters;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
 
 @Configuration
 public class HttpConfig {
+    private static final Log log = LogFactory.getLog(HttpConfig.class);
     
     @Autowired
     AppProperties appProperties; 
-    @Autowired
-    private SSLContextParameters sslContextParameters;
     @Autowired
     private NoopHostnameVerifier noopHostnameVerifier;
     @Autowired
     private CamelContext camelContext;
     
-    
+    @Value("${fbiEbtsAdapter.truststoreLocation}")
+    private String truststoreLocation; 
+    @Value("${fbiEbtsAdapter.truststorePassword}")
+    private String truststorePassword; 
+    @Value("${fbiEbtsAdapter.fbi.keyPassword}")
+    private String fbiKeyPassword; 
+    @Value("${fbiEbtsAdapter.fbi.keystoreLocation}")
+    private String fbiKeystoreLocation; 
+    @Value("${fbiEbtsAdapter.fbi.keystorePassword}")
+    private String fbiKeystorePassword; 
+
     @Bean
-    public PoolingHttpClientConnectionManager poolingHttpClientConnectionManager() {
-        PoolingHttpClientConnectionManager manager = new PoolingHttpClientConnectionManager(15, TimeUnit.SECONDS);
-        manager.setMaxTotal(appProperties.getHttpMaxTotalConnections());
-        manager.setDefaultMaxPerRoute(appProperties.getHttpConnectionsPerRoute());
-        manager.setValidateAfterInactivity(appProperties.getHttpValidateAfterInactivity());
-        return manager;
+    SSLContextParameters sslContextParameters(AppProperties props) {
+        KeyStoreParameters trustStore = new KeyStoreParameters();
+        trustStore.setResource(truststoreLocation);
+        trustStore.setPassword(truststorePassword);
+
+        TrustManagersParameters trustManagers = new TrustManagersParameters();
+        trustManagers.setKeyStore(trustStore);
+
+        KeyStoreParameters keyStore = new KeyStoreParameters();
+        keyStore.setResource(fbiKeystoreLocation);
+        keyStore.setPassword(fbiKeystorePassword);
+
+        KeyManagersParameters keyManagers = new KeyManagersParameters();
+        keyManagers.setKeyStore(keyStore);
+        keyManagers.setKeyPassword(fbiKeyPassword);
+
+        SSLContextParameters sslContextParameters = new SSLContextParameters();
+        sslContextParameters.setTrustManagers(trustManagers);
+        sslContextParameters.setKeyManagers(keyManagers);
+
+        return sslContextParameters;
     }
+    
+//    @Bean
+//    PoolingHttpClientConnectionManager poolingHttpClientConnectionManager() {
+//        PoolingHttpClientConnectionManager manager = new PoolingHttpClientConnectionManager(15, TimeUnit.SECONDS);
+//        manager.setMaxTotal(appProperties.getHttpMaxTotalConnections());
+//        manager.setDefaultMaxPerRoute(appProperties.getHttpConnectionsPerRoute());
+//        manager.setValidateAfterInactivity(appProperties.getHttpValidateAfterInactivity());
+//        manager.closeIdleConnections(15, TimeUnit.SECONDS);
+//        return manager;
+//    }
     
     @Bean("https")
-    HttpComponent httpComponent(PoolingHttpClientConnectionManager manager) {
-        
-        // Configure the builder directly
-        HttpClientConfigurer configurer = (HttpClientBuilder clientBuilder) -> {
-            try {
-                clientBuilder
-                    .setConnectionManager(manager)
-                    .setSSLContext(sslContextParameters.createSSLContext(camelContext))
-                    .setSSLHostnameVerifier(noopHostnameVerifier)
-                    .setKeepAliveStrategy((response, context) -> {
-                        return appProperties.getHttpConnectionTimeToLive();
-                    })
-                    .evictIdleConnections(appProperties.getHttpIdleConnectionTimeout(), TimeUnit.SECONDS)
-                    .setDefaultRequestConfig(RequestConfig.custom()
-                            .setConnectionRequestTimeout(appProperties.getHttpConnectionRequestTimeout())
-                            .setConnectTimeout(appProperties.getHttpConnectionTimeout())
-                            .setSocketTimeout(appProperties.getHttpSocketTimeout())
-                            .build());
-            } catch (GeneralSecurityException | IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        };
-
-        HttpComponent comp = new HttpComponent();
-        comp.setHttpClientConfigurer(configurer);
+    HttpComponent httpsComponent(SSLContextParameters sslContextParameters) {
+     HttpComponent comp = camelContext.getComponent("https", HttpComponent.class);
+        comp.setMaxTotalConnections(appProperties.getHttpMaxTotalConnections());
+        comp.setConnectionsPerRoute(appProperties.getHttpConnectionsPerRoute());
+        comp.setConnectionRequestTimeout(appProperties.getHttpConnectionRequestTimeout());
+        comp.setSocketTimeout(appProperties.getHttpSocketTimeout());
+        comp.setConnectionTimeToLive(appProperties.getHttpConnectionTimeToLive());
+        comp.setSslContextParameters(sslContextParameters);  
+        comp.setX509HostnameVerifier(noopHostnameVerifier);
+        log.info("Custom HTTPS Component initialized with truststore and keystore: " + 
+                sslContextParameters.getTrustManagers().getKeyStore().getResource() + "\t" + 
+                sslContextParameters.getKeyManagers().getKeyStore().getResource()); 
         return comp;
     }
-
-    @Scheduled(fixedDelay = 15000)
-    public void evictIdleConnections(PoolingHttpClientConnectionManager manager) {
-        manager.closeExpiredConnections();
-        manager.closeIdleConnections(appProperties.getHttpIdleConnectionTimeout(), TimeUnit.SECONDS);
-    }
-}
+    
+}    
